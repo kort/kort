a1759b1373a2fb689d99e65a6037780e7c842b61
Stefan Oderbolz <oderbolz@gmail.com>
2012-09-28 14:18:01 +0200

    Updated redmine installation and backup scripts

---
+++ redmine/sync_backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script syncs the remotely created backup files (see redmine_backup.sh) to another machine
+rsync -avz <user>@<host>:/home/redmine/backup/* /path/to/backup/location
