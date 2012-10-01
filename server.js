7e68109ce5a4e9e667cdfa16e9660a31e65d9ef1
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-01 12:09:04 +0200

    Cleanup of server.js, Port 8080

--- server.js
+++ server.js
@@ -6,44 +6,11 @@ var http = require('http'),
 		fs   = require('fs');
 var connect = require('connect');
 
-/*
-var server = Connect.createServer(
-	Connect.logger(), // Log responses to the terminal using Common Log Format.
-	//Connect.responseTime(), // Add a special header with timing information.
-	//Connect.conditionalGet(), // Add HTTP 304 responses to save even more bandwidth.
-	//Connect.cache(), // Add a short-term ram-cache to improve performance.
-	//Connect.gzip(), // Gzip the output stream when the browser wants it.
-	Connect.staticProvider(__dirname) // Serve all static files in the current dir.
-);
-*/
 var app = connect()
   .use(connect.logger('dev'))
   .use(connect.static(__dirname))
   .use(function(req, res){
     res.end('hello world\n');
-  })
- .listen(3000);
-/*
-var socket = io.listen(server);
+  });
 
-socket.on('connection', function(client) {
-
-	var user;
-
-	client.on('message', function(message) {
-		if (!user) {
-			user = message;
-			client.send({ message: 'Welcome, ' + user.nickname + '!', nickname: 'server', gravatar: '' });
-			return;
-		}
-		var response = {
-			'nickname': user.nickname,
-			'gravatar': user.gravatar,
-			'message': message.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;")
-		};
-		socket.broadcast(response);
-	});
-
-});
-*/
-//server.listen(4000);
+http.createServer(app).listen(8080);
