bc4a3c8c4ec8e19b162a6e3b8179046279dce518
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-05 11:39:25 +0200

    Replace * with trailing slash

--- redmine/sync_backup.sh
+++ redmine/sync_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script syncs the remotely created backup files (see redmine_backup.sh) to another machine
-rsync -avz <user>@<host>:/home/redmine/backup/* /path/to/backup/location
+rsync -avz <user>@<host>:/home/redmine/backup/ /path/to/backup/location
