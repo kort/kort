c46cbd45bbb2d5bab2003b9b221a78efb36967ed
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-07 12:57:20 +0200

    Created examples for markers, linestrings and popups

--- app/controller/Map.js
+++ app/controller/Map.js
@@ -3,38 +3,113 @@ Ext.define('OpenLayersApp.controller.Map', {
 	
     config: {
         refs: {
-			openLayersMap: '#openlayersmap'
+			mapCmp: '#openlayersmap'
         },
 		control: {
-			openLayersMap: {
+			mapCmp: {
 				maprender: 'onMapRender'
 			}
 		},
 		
-		markersLayer: null,
-		markers: []
+		map: null,
+		vectorLayer: null,
+		features: [],
+		selectControl: null
     },
 	
 	onMapRender: function(component, map, layer) {
-		this.createMarkersLayer(map);
+		var me = this;
+		me.setMap(map);
+		var vectorLayer = this.createVectorLayer(map);
 		
-		var size = new OpenLayers.Size(21,25);
-		var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-		var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
-		var lonlat = new OpenLayers.LonLat(component.getGeo().getLongitude(), component.getGeo().getLatitude());
+		// adding marker to vector layer
+		var markerPoint = new OpenLayers.Geometry.Point(component.getGeo().getLongitude(), component.getGeo().getLatitude());
 		
-		this.addMarker(lonlat, icon);
+		var iconWidth = 21;
+		var iconHeight = 25;
+		
+		var iconStyle = {
+			externalGraphic: "http://www.openlayers.org/dev/img/marker.png",
+			graphicWidth: iconWidth,
+			graphicHeight: iconHeight,
+			graphicYOffset: -(iconHeight)
+		};
+		
+		me.addMarker(markerPoint, null, iconStyle);
+		
+		// adding linestring
+		var point1 = new OpenLayers.Geometry.Point(component.getGeo().getLongitude(), component.getGeo().getLatitude());
+		var point2 = new OpenLayers.Geometry.Point(component.getGeo().getLongitude() + 1, component.getGeo().getLatitude() + 1);
+		
+		var lineStyle = { 
+			strokeColor: '#0000ff', 
+			strokeOpacity: 0.5,
+			strokeWidth: 5
+		};
+		
+		me.addLineString(point1, point2, null, lineStyle);
+		
+		// popup
+		var selector = new OpenLayers.Control.SelectFeature(vectorLayer, {
+			autoActivate: true
+		}); 
+    
+		map.addControl(selector);
 	},
 	
-	createMarkersLayer: function(map) {
-		this.setMarkersLayer(new OpenLayers.Layer.Markers("Markers"));
-		map.addLayer(this.getMarkersLayer());
+	onFeatureSelected: function(scope, feature, map) {
+		var popup = new OpenLayers.Popup.FramedCloud("popup",
+			feature.geometry.getBounds().getCenterLonLat(),
+			null,
+			feature.id,
+			null,
+			true,
+			null
+		);
+		popup.autoSize = true;
+		popup.maxSize = new OpenLayers.Size(400,800);
+		popup.fixedRelativePosition = true;
+		feature.popup = popup;
+		map.addPopup(popup);
 	},
 	
-	addMarker: function(position, icon) {
-		this.getOpenLayersMap().transformLonLatObject(position);
-		var marker = new OpenLayers.Marker(position, icon);
-		this.getMarkers().push(marker);
-		this.getMarkersLayer().addMarker(marker);
+	onFeatureUnselected: function(scope, feature, map) {
+		map.removePopup(feature.popup);
+		feature.popup.destroy();
+		feature.popup = null;
+	},
+	
+	createVectorLayer: function(map) {
+		var me = this;
+		var vectorLayer = new OpenLayers.Layer.Vector("Vector Layer", {
+			eventListeners: {
+				featureselected: function(event) {
+					me.onFeatureSelected(me, event.feature, map);
+				},
+				featureunselected: function(event){
+					me.onFeatureUnselected(me, event.feature, map);
+				}
+			}
+		});
+		
+		this.setVectorLayer(vectorLayer);
+		map.addLayer(vectorLayer);
+		return vectorLayer;
+	},
+	
+	addMarker: function(point, attributes, style) {
+		this.getMapCmp().transformLonLatObject(point);
+		var iconFeature = new OpenLayers.Feature.Vector(point, attributes, style);
+		this.getFeatures().push(iconFeature);
+		this.getVectorLayer().addFeatures(iconFeature);
+	},
+	
+	addLineString: function(startPoint, endPoint, attributes, style) {
+		this.getMapCmp().transformLonLatObject(startPoint);
+		this.getMapCmp().transformLonLatObject(endPoint);
+		var lineString = new OpenLayers.Geometry.LineString([startPoint, endPoint]);
+		var lineFeature = new OpenLayers.Feature.Vector(lineString, attributes, style);
+		this.getFeatures().push(lineFeature);
+		this.getVectorLayer().addFeatures(lineFeature);
 	}
 });
