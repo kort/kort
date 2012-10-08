829c2f1cb872a0d5529f8e19267a662a6dc1abad
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-08 09:52:41 +0200

    Added a few new rules like indent with 4 spaces, no unused variables permitted etc.

--- grunt.js
+++ grunt.js
@@ -1,62 +1,66 @@
 /*global module:false*/
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    qunit: {
-      files: ['test/index.html']
-    },
-	lint: {
-      src: 'app/**/*.js',
-      grunt: 'grunt.js',
-      tests: 'test/**/*.js'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-		devel: true
-      },
-      globals: {
-		Ext: true,
-		OpenLayers: true
-      },
-      tests: {
-        globals: {
-            Ext            : true,
-            OpenLayers     : true,
-			asyncTest      : true,
-			deepEqual      : true,
-			equal          : true,
-			expect         : true,
-			module         : true,
-			notDeepEqual   : true,
-			notEqual       : true,
-			notStrictEqual : true,
-			ok             : true,
-			QUnit          : true,
-			raises         : true,
-			start          : true,
-			stop           : true,
-			strictEqual    : true,
-			test           : true
+    
+    // Project configuration.
+    grunt.initConfig({
+        qunit: {
+            files: ['test/index.html']
+        },
+        lint: {
+            src: 'app/**/*.js',
+            grunt: 'grunt.js',
+            tests: 'test/**/*.js'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                devel: true,
+                trailing: true,
+                maxcomplexity: 3,
+                indent: 4
+            },
+            globals: {
+            Ext: true,
+            OpenLayers: true
+            },
+            tests: {
+                globals: {
+                    Ext: true,
+                    OpenLayers: true,
+                    asyncTest: true,
+                    deepEqual: true,
+                    equal: true,
+                    expect: true,
+                    module: true,
+                    notDeepEqual: true,
+                    notEqual: true,
+                    notStrictEqual: true,
+                    ok: true,
+                    QUnit: true,
+                    raises: true,
+                    start: true,
+                    stop: true,
+                    strictEqual: true,
+                    test: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+    // Default task.
+    grunt.registerTask('default', 'lint qunit concat min');
 
-  // Travis CI task.
-  grunt.registerTask('travis', 'lint qunit');
+    // Travis CI task.
+    grunt.registerTask('travis', 'lint qunit');
 };
