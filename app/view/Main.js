8109b294ec82f5537bc9163254d02730f9c0ed55
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-08 14:35:46 +0200

    used i18n text from properties file

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,42 +1,42 @@
 Ext.define('OpenLayersApp.view.Main', {
     extend: 'Ext.tab.Panel',
     xtype: 'main',
-	id: 'mainTabPanel',
-	requires: ['Ext.ux.OpenLayersMap'],
-	
+    id: 'mainTabPanel',
+    requires: ['Ext.ux.OpenLayersMap'],
+
     config: {
-		tabBar: {
-			docked: 'bottom',
-			layout: {
-				pack: 'start'
-			}
-		},
+        tabBar: {
+            docked: 'bottom',
+            layout: {
+                pack: 'start'
+            }
+        },
 
         items: [
             {
-                title: 'OpenStreetMap',
+                title: Ext.i18n.Bundle.message('tabpanel.map'),
                 iconCls: 'maps',
-				layout: 'fit',
+                layout: 'fit',
 
                 items: [
-					{
-						xtype: 'openlayersmap',
-						id: 'openlayersmap',
-						useCurrentLocation: true,
-						autoMapCenter: false,
-						mapOptions: {
-							zoom: 15,
-							controls: [
-								new OpenLayers.Control.TouchNavigation({
-									dragPanOptions: {
-										enableKinetic: true
-									}
-								}),
-								new OpenLayers.Control.Zoom()
-							]
-						}
-					}
-				]
+                    {
+                        xtype: 'openlayersmap',
+                        id: 'openlayersmap',
+                        useCurrentLocation: true,
+                        autoMapCenter: false,
+                        mapOptions: {
+                            zoom: 15,
+                            controls: [
+                                new OpenLayers.Control.TouchNavigation({
+                                    dragPanOptions: {
+                                        enableKinetic: true
+                                    }
+                                }),
+                                new OpenLayers.Control.Zoom()
+                            ]
+                        }
+                    }
+                ]
             }
         ]
     }
