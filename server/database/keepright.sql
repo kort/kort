490fa80b7978aaec596db08f3ecb23cc58ead995
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-11 18:42:34 +0200

    Added database setup and schema

---
+++ server/database/keepright.sql
@@ -0,0 +1,29 @@
+create schema keepright;
+
+create type state as enum('new','reopened','ignore_temporarily','ignore');
+
+create type osm_type as enum('node','way','relation');
+
+create table keepright.errors (
+  schema varchar(6) not null default '',
+  error_id integer not null,
+  error_type integer not null,
+  error_name varchar(100) not null,
+  object_type osm_type not null,
+  object_id bigint not null,
+  state state not null,
+  first_occurrence timestamp not null,
+  last_checked timestamp not null,
+  object_timestamp timestamp not null,
+  user_name text not null,
+  lat integer not null,
+  lon integer not null,
+  comment text,
+  comment_timestamp timestamp,
+  msgid text,
+  txt1 text,
+  txt2 text,
+  txt3 text,
+  txt4 text,
+  txt5 text
+);
---
+++ server/database/setup_db.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+while getopts ":o:n:dp" opt; do
+    case $opt in
+        o)  
+            DB_OWNER="$OPTARG"
+            ;;
+        n)  
+            DB_NAME="$OPTARG"
+            ;;
+        d)  
+            DROP_DB="true"
+            ;;
+        p)  
+            PREVIOUS_DOWNLOAD="true"
+            ;;
+        \?) # fall-through
+            ;&
+        :)  
+            echo "USAGE: `basename $0` [-o <db owner>] [-n <database name>] [-d drop database if exists] [-l use previous download]" >&2
+            echo "Example: `basename $0` -o `whoami` -n osm_bugs -d" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $DB_NAME ] ; then
+    DB_NAME="osm_bugs"
+fi
+
+if [ -z $DB_OWNER ] ; then
+    DB_OWNER=`whoami`
+fi
+
+
+if [[ $DROP_DB ]] ; then
+    echo "Dropping database $DB_NAME"
+    psql -c "drop database if exists $DB_NAME;"
+fi
+
+# Create database
+echo "Create database $DB_NAME (Owner: $DB_OWNER)"
+createdb -E UTF8 -O $DB_OWNER $DB_NAME
+psql -c "alter schema keepright owner to $DB_OWNER;"
+psql -d $DB_NAME -f keepright.sql
+
+# Load keepright data
+if [ -z $PREVIOUS_DOWNLOAD ] ; then
+    wget -O - http://keepright.ipax.at/keepright_errors.txt.bz2 | bzcat | sed -i 's/\\N/NULL/g' > /tmp/keepright_errors.txt
+fi
+echo "Start loading data"
+psql -d $DB_NAME -c "\copy keepright.errors from '/tmp/keepright_errors.txt' DELIMITER '	' null 'NULL' CSV HEADER;"
+echo "End."
