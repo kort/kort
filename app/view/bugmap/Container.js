9acd505bff8e88fffb0da6401e0cd7abea10f062
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-14 11:25:15 +0200

    extracted bugmap to own namespace

---
+++ app/view/bugmap/Container.js
@@ -0,0 +1,35 @@
+Ext.define('Kort.view.bugmap.Container', {
+	extend: 'Ext.Container',
+	alias: 'widget.bugmapcontainer',
+    requires: ['Ext.ux.LeafletMap'],
+	
+	config: {
+		title: 'Bugs',
+		url: 'bugmap',
+		id: 'bugmapContainer',
+		iconCls: 'maps',
+		layout: 'fit',
+		items: [
+			{
+				xtype: 'titlebar',
+				cls: 'titlebar',
+				docked: 'top',
+				title: 'Bugs'
+			},
+			{
+                xtype: 'leafletmap',
+                id: 'bugmap',
+                useCurrentLocation: true,
+                autoMapCenter: false,
+                mapOptions: {
+                    zoom: 15
+                },
+                tileLayerUrl: 'http://{s}.tile.cloudmade.com/{apikey}/{styleId}/256/{z}/{x}/{y}.png',
+                tileLayerOptions: {
+                    apikey: '729242682cb24de8aa825c8aed993cba',
+                    styleId: 997
+                }
+			}
+		]
+	}
+});
