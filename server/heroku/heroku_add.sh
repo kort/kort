b74aa2a794f36bfc9d32c6b14f56b1cfa0d15115
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-15 09:47:10 +0200

    Extracted heroku scripts for build

---
+++ server/heroku/heroku_add.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git checkout -b travis
+git add .
+git commit -m "Travis CI commit of files added/modified from build"
--- server/heroku/heroku_clean.sh
+++ server/heroku/heroku_clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git rm -r _*
+git rm -r _D*
 git rm -r touch/src/
 git rm -r touch/sencha-touch-*
 git rm .gitignore
