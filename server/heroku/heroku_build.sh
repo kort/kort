93c6137ed05a2dbe5b7f2ace006c604756835d7a
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-15 17:54:34 +0200

    Adopeted scripts to use environment variables for paths

--- server/heroku/heroku_build.sh
+++ server/heroku/heroku_build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-mkdir build-heroku
-mkdir build-heroku/lib
+if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
+   echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
+   exit 1
+fi
 
-cp index.* build-heroku/
-cp app.* build-heroku/
-cp -r app build-heroku/
-cp -r resources build-heroku/
-cp -r touch build-heroku/
-cp -r ux build-heroku/
-cp -r i18n build-heroku
+mkdir $BUILD_DIR
+mkdir $BUILD_DIR/lib
 
-cp -r lib/Leaflet-0.4.4/ build-heroku/lib/
+cp $CI_HOME/index.* $BUILD_DIR
+cp $CI_HOME/app.* $BUILD_DIR/
+cp -r $CI_HOME/app $BUILD_DIR/
+cp -r $CI_HOME/resources $BUILD_DIR/
+cp -r $CI_HOME/touch $BUILD_DIR/
+cp -r $CI_HOME/ux $BUILD_DIR/
+cp -r $CI_HOME/i18n $BUILD_DIR
 
-mv build-heroku ../
+cp -r $CI_HOME/lib/Leaflet-0.4.4/ $BUILD_DIR/lib/
+
+mv $BUILD_DIR ../
+cd ../$BUILD_DIR
--- server/heroku/heroku_keys.sh
+++ server/heroku/heroku_keys.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -z $CI_HOME ] ; then
+   echo "You need to specify the CI_HOME environment variable."
+   exit 1
+fi
+
 heroku keys:clear
 yes | heroku keys:add
-heroku keys:add server/ssh_pub_keys/id_ras_odi.pub
+heroku keys:add $CI_HOME/server/ssh_pub_keys/id_ras_odi.pub
