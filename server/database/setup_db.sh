1bf0c70c4dfcb717cf516f4bc30e92ead91e126f
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-17 01:49:06 +0200

    Added splitting and filtering of input data from keepright

--- server/database/setup_db.sh
+++ server/database/setup_db.sh
@@ -16,7 +16,7 @@ while getopts ":o:n:dp" opt; do
         \?) # fall-through
             ;&
         :)  
-            echo "USAGE: `basename $0` [-o <db owner>] [-n <database name>] [-d drop database if exists] [-l use previous download]" >&2
+            echo "USAGE: `basename $0` [-o <db owner>] [-n <database name>] [-d drop database if exists] [-p use previous download]" >&2
             echo "Example: `basename $0` -o `whoami` -n osm_bugs -d" >&2
             exit 1
             ;;
@@ -40,13 +40,20 @@ fi
 # Create database
 echo "Create database $DB_NAME (Owner: $DB_OWNER)"
 createdb -E UTF8 -O $DB_OWNER $DB_NAME
-psql -c "alter schema keepright owner to $DB_OWNER;"
 psql -d $DB_NAME -f keepright.sql
+psql -c "alter schema keepright owner to $DB_OWNER;"
 
 # Load keepright data
 if [ -z $PREVIOUS_DOWNLOAD ] ; then
-    wget -O - http://keepright.ipax.at/keepright_errors.txt.bz2 | bzcat | sed -i 's/\\N/NULL/g' > /tmp/keepright_errors.txt
+    wget -O - http://keepright.ipax.at/keepright_errors.txt.bz2 | bzcat | grep -v -f ignore_errors.txt > /tmp/keepright_errors.txt
+    echo "Splitting CSV in parts..."
+    split -l 200000 /tmp/keepright_errors.txt /tmp/kr_part
+    rm /tmp/keepright_errors.txt
 fi
 echo "Start loading data"
-psql -d $DB_NAME -c "\copy keepright.errors from '/tmp/keepright_errors.txt' DELIMITER '	' null 'NULL' CSV HEADER;"
+for part_file in /tmp/kr_part*
+do
+    echo $part_file
+    psql -d $DB_NAME -c "copy keepright.errors from '$part_file' DELIMITER '	' null '\N' CSV HEADER;"
+done
 echo "End."
