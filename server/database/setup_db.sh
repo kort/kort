f8ac56437032f863fef34213643835be888f9e9a
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-17 08:13:51 +0200

    Added new option to specify file for import

--- server/database/setup_db.sh
+++ server/database/setup_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts ":o:n:dp" opt; do
+while getopts ":o:n:dp:" opt; do
     case $opt in
         o)  
             DB_OWNER="$OPTARG"
@@ -11,13 +11,13 @@ while getopts ":o:n:dp" opt; do
             DROP_DB="true"
             ;;
         p)  
-            PREVIOUS_DOWNLOAD="true"
+            PREVIOUS_DOWNLOAD="$OPTARG"
             ;;
         \?) # fall-through
             ;&
         :)  
-            echo "USAGE: `basename $0` [-o <db owner>] [-n <database name>] [-d drop database if exists] [-p use previous download]" >&2
-            echo "Example: `basename $0` -o `whoami` -n osm_bugs -d" >&2
+            echo "USAGE: `basename $0` [-o <db owner>] [-n <database name>] [-d drop database if exists] [-p path to previously downloaded error csv]" >&2
+            echo "Example: `basename $0` -o `whoami` -n osm_bugs -d -p /tmp/keepright_errors.txt" >&2
             exit 1
             ;;
     esac
@@ -31,6 +31,11 @@ if [ -z $DB_OWNER ] ; then
     DB_OWNER=`whoami`
 fi
 
+if [[ $PREVIOUS_DOWNLOAD && ! -f $PREVIOUS_DOWNLOAD ]] ; then
+    echo "Previous download '$PREVIOUS_DOWNLOAD' does not exist or is not a regular file."
+    exit 1
+fi
+
 
 if [[ $DROP_DB ]] ; then
     echo "Dropping database $DB_NAME"
@@ -40,17 +45,22 @@ fi
 # Create database
 echo "Create database $DB_NAME (Owner: $DB_OWNER)"
 createdb -E UTF8 -O $DB_OWNER $DB_NAME
+psql -d $DB_NAME -c "create schema keepright authorization $DB_OWNER"
 psql -d $DB_NAME -f keepright.sql
-psql -c "alter schema keepright owner to $DB_OWNER;"
+psql -d $DB_NAME -c "alter table keepright.errors owner to $DB_OWNER"
 
 # Load keepright data
 if [ -z $PREVIOUS_DOWNLOAD ] ; then
     wget -O - http://keepright.ipax.at/keepright_errors.txt.bz2 | bzcat | grep -v -f ignore_errors.txt > /tmp/keepright_errors.txt
-    echo "Splitting CSV in parts..."
-    rm /tmp/kr_part*
-    split -l 200000 /tmp/keepright_errors.txt /tmp/kr_part
-    rm /tmp/keepright_errors.txt
+else
+    cp $PREVIOUS_DOWNLOAD /tmp/keepright_errors.txt
 fi
+
+echo "Splitting CSV in parts..."
+rm /tmp/kr_part*
+split -l 200000 /tmp/keepright_errors.txt /tmp/kr_part
+rm /tmp/keepright_errors.txt
+
 echo "Start loading data"
 for part_file in /tmp/kr_part*
 do
