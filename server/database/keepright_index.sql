fbb7362f8f5ed4edca3aee79f1d58988c5356a97
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-18 13:32:14 +0200

    Added script for keepright indices

---
+++ server/database/keepright_index.sql
@@ -0,0 +1,13 @@
+CREATE INDEX "error_id#idx"
+   ON keepright.errors (error_id ASC NULLS LAST);
+CREATE INDEX "lat#idx"
+   ON keepright.errors (lat ASC NULLS LAST);
+CREATE INDEX "lon#idx"
+   ON keepright.errors (lon ASC NULLS LAST);
+CREATE INDEX "object_type#idx"
+   ON keepright.errors (object_type ASC NULLS LAST);
+CREATE INDEX "state#idx"
+   ON keepright.errors (state ASC NULLS LAST);
+CREATE INDEX "error_type#idx"
+   ON keepright.errors (error_type ASC NULLS LAST);
+
