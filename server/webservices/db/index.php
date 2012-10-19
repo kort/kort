e7d1b26196dae751a75fc223cb4bd01fe8f3c3b4
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-19 15:54:07 +0200

    used correct values for lat/lng bounds

--- server/webservices/db/index.php
+++ server/webservices/db/index.php
@@ -12,20 +12,22 @@ $app = new \Slim\Slim();
 
 // define REST resources
 $app->get('/', 'rootRouteHandler');
-$app->get('/bugs/:id', 'bugsIdRouteHandler');
+$app->get('/bugs/:schema/:id', 'bugsIdRouteHandler');
 $app->get('/bugs/bounds/:northeastlat,:northeastlng/:southwestlat,:southwestlng', 'bugsBoundsRouteHandler');
 
 function rootRouteHandler() {
     echo "You're on the rooooot";
 }
 
-function bugsIdRouteHandler($id) {
+function bugsIdRouteHandler($schema, $id) {
     $dbConfig = new PsqlDbConfig();
     $psqlHelper = new PsqlHelper($dbConfig);
-    $result = $psqlHelper->doSelectQuery('error_id = '.$id);
+    $result = $psqlHelper->doSelectQuery('schema = \''.$schema.'\' AND error_id = '.$id);
     
     // only return first row (error_id not unique)
-    echo json_encode($result[0]);
+    if(count($result) > 0) {
+        echo json_encode($result[0]);
+    }
     $psqlHelper->close();
 }
 
@@ -33,11 +35,20 @@ function bugsBoundsRouteHandler($northEastLat, $northEastLng, $southWestLat, $so
     $dbConfig = new PsqlDbConfig();
     $psqlHelper = new PsqlHelper($dbConfig);
     
+    $northEastLat = convertLatLngValue($northEastLat);
+    $southWestLat = convertLatLngValue($southWestLat);
+    $northEastLng = convertLatLngValue($northEastLng);
+    $southWestLng = convertLatLngValue($southWestLng);
+    
     $where = 'lat < '.$northEastLat.' AND lat > '.$southWestLat. ' AND lon < '.$northEastLng.' AND lon > '.$southWestLng;
     $result = $psqlHelper->doSelectQuery($where);
     
     echo json_encode($result);
     $psqlHelper->close();
+}
+
+function convertLatLngValue($value) {
+    return round($value * 10000000); 
 }
 
 // start Slim app
