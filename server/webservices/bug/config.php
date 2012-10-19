979d38363675832d10501fe220e7584c6571b88f
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-19 11:19:45 +0200

    Added bug webservice which relays to our db webservice

---
+++ server/webservices/bug/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * setup the url path to the db webservice
+ */
+class DbWebserviceConfig {
+    var $url = "http://kort.rdmr.ch/webservices/db";
+    //var $url = "http://localhost/kort/server/webservices/db";
+}
+?>
