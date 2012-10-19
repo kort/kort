b727f9bc7021034003e45a637bb3b83b01290a3c
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-19 23:46:32 +0200

    Added PSR-2 ruleset to the build and corrected most errors in php files

--- server/webservices/bug/config.php
+++ server/webservices/bug/config.php
@@ -1,8 +1,10 @@
 <?php
+namespace Kort\Webservice;
+
 /**
  * setup the url path to the db webservice
  */
-class DbWebserviceConfig {
-    var $url = "http://kort.rdmr.ch/webservices/db";
+class DbWebserviceConfig
+{
+    public $url = "http://kort.rdmr.ch/webservices/db";
 }
-?>
---
+++ server/webservices/db/RouteHandler.php
@@ -0,0 +1,61 @@
+<?php
+namespace Kort\Webservice;
+
+use Kort\Webservice\PsqlHelper;
+use Kort\Webservice\DbConfig;
+
+class RouteHandler
+{
+    protected $db;
+
+    public static function autoload($className)
+    {
+        $className = str_replace(__NAMESPACE__.'\\', '', $className);
+        echo $className;
+        require_once($className.".php");
+    }
+
+    public function __construct()
+    {
+        spl_autoload_register(__NAMESPACE__ . "\\RouteHandler::autoload");
+        $this->db = new PsqlHelper(new DbConfig());
+    }
+
+    public function rootRouteHandler()
+    {
+        echo "You're on the rooooot";
+    }
+
+    public function bugsIdRouteHandler($schema, $id)
+    {
+        $result = $this->db->doSelectQuery('schema = \'' . $schema . '\' AND error_id = ' . $id);
+
+        // only return first row (error_id not unique)
+        if (count($result) > 0) {
+            echo json_encode($result[0]);
+        }
+        $this->db->close();
+    }
+
+    public function bugsBoundsRouteHandler($northEastLat, $northEastLng, $southWestLat, $southWestLng)
+    {
+        $northEastLat = convertLatLngValue($northEastLat);
+        $southWestLat = convertLatLngValue($southWestLat);
+        $northEastLng = convertLatLngValue($northEastLng);
+        $southWestLng = convertLatLngValue($southWestLng);
+
+        $where  = 'lat < ' . $northEastLat;
+        $where .= ' AND lat > ' . $southWestLat;
+        $where .= ' AND lon < ' . $northEastLng;
+        $where .= ' AND lon > ' . $southWestLng;
+        $result = $this->db->doSelectQuery($where);
+
+        echo json_encode($result);
+        $this->db->close();
+    }
+
+    private function convertLatLngValue($value)
+    {
+        return round($value * 10000000);
+    }
+}
