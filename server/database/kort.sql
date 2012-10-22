24fd25a623b8940751f9504064db95dec97b1e8e
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-22 13:34:12 +0200

    added create_date field to fix table

--- server/database/kort.sql
+++ server/database/kort.sql
@@ -1,9 +1,11 @@
+drop schema kort cascade;
 create schema kort;
 
 create table kort.fix (
     id integer primary key,
+    create_date timestamp not null,
     error_id integer not null,
     message text
 );
 
-create sequence serial;
+create sequence kort.fix_id;
