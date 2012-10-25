101af5d52db76280dd34faf33cac06f3e6447282
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-25 11:27:01 +0200

    implemented bug loading behaviour

--- app/model/Bug.js
+++ app/model/Bug.js
@@ -5,11 +5,14 @@ Ext.define('Kort.model.Bug', {
 		
         fields: [
 			{ name: 'id', type: 'auto' },
+			{ name: 'schema', type: 'string' },
+			{ name: 'type', type: 'string' },
+			{ name: 'osm_id', type: 'int' },
+			{ name: 'osm_type', type: 'string' },
 			{ name: 'title', type: 'string' },
 			{ name: 'description', type: 'string' },
             { name: 'latitude', type: 'string' },
-            { name: 'longitude', type: 'string' },
-			{ name: 'type', type: 'string' }
+            { name: 'longitude', type: 'string' }
         ]
     }
 });
