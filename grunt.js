88b4f524d4f06db86952e4e51e22846768aefd67
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-29 10:31:38 +0100

    Added white: false option

--- grunt.js
+++ grunt.js
@@ -29,6 +29,7 @@ module.exports = function(grunt) {
                 browser: true,
                 devel: true,
                 trailing: true,
+                white:  false,
                 maxcomplexity: 3,
                 indent: 4
             },
