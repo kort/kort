ceec692e53141a75b6236a653d0a5074d092419c
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-29 09:26:41 +0100

    Added Google API PHP client

--- server/heroku/heroku_build.sh
+++ server/heroku/heroku_build.sh
@@ -7,6 +7,7 @@ fi
 mkdir $BUILD_DIR
 mkdir -p $BUILD_DIR/lib/Leaflet-0.4.5
 mkdir -p $BUILD_DIR/lib/Slim-2.1.0
+mkdir -p $BUILD_DIR/lib/google-api-php-client
 mkdir -p $BUILD_DIR/server
 
 cp $CI_HOME/index.* $BUILD_DIR
@@ -20,6 +21,7 @@ cp -r $CI_HOME/test $BUILD_DIR
 
 cp -r $CI_HOME/lib/Leaflet-0.4.5/dist/ $BUILD_DIR/lib/Leaflet-0.4.5/
 cp -r $CI_HOME/lib/Slim-2.1.0/Slim/ $BUILD_DIR/lib/Slim-2.1.0/
+cp -r $CI_HOME/lib/google-api-php-client/src $BUILD_DIR/lib/google-api-php-client/
 cp -r $CI_HOME/lib/qunit $BUILD_DIR/lib
 cp -r $CI_HOME/lib/simpletest $BUILD_DIR/lib
 
