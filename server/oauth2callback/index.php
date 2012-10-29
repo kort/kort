03e2674e825c2baf07a240ce3a5dde9137231ce8
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-29 17:31:02 +0100

    Handle OAuth response with Google API client

--- server/oauth2callback/index.php
+++ server/oauth2callback/index.php
@@ -1,2 +1,16 @@
 <?php
-print_r($_GET);
+require_once "../../lib/google-api-php-client/src/Google_Client.php";
+
+$client_id = '653755350671.apps.googleusercontent.com';
+$client_secret = '7o67nw_e1diHMK72Q_GJlBsg';
+
+$client = new Google_Client();
+$client->setApplicationName("kort");
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['token'] = $client->getAccessToken();
+  header('Location: http://' . $_SERVER['HTTP_HOST']);
+}
