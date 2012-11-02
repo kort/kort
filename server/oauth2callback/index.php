170e0ed099751b73fd0337643f88963dd8174236
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-02 14:22:19 +0100

    Handle OAuth response and save data in session

--- server/oauth2callback/index.php
+++ server/oauth2callback/index.php
@@ -1,21 +1,22 @@
 <?php
 require_once "../../lib/google-api-php-client/src/Google_Client.php";
+require_once '../../lib/google-api-php-client/src/contrib/Google_Oauth2Service.php';
 
 $client = new Google_Client();
-
-print_r($_GET);
+$oauth2 = new Google_Oauth2Service($client);
 
 if (isset($_GET['code'])) {
     $client->authenticate($_GET['code']);
     session_start();
     $_SESSION['token'] = $client->getAccessToken();
-
-    //save refresh token in DB
-
-    $appUrl = 'http://' . $_SERVER['HTTP_HOST'];
-    if ($_SERVER['HTTP_HOST'] === "localhost") {
-        $appUrl .= "/kort";
-    }
-
-    header('Location: ' . $appUrl);
+    $user = $oauth2->userinfo->get();
+    $_SESSION['email'] = filter_var($user['email'], FILTER_SANITIZE_EMAIL);
+    $_SESSION['name'] = $user['name'];
 }
+
+$appUrl = 'http://' . $_SERVER['HTTP_HOST'];
+if ($_SERVER['HTTP_HOST'] === "localhost") {
+    $appUrl .= "/kort";
+}
+
+header('Location: ' . $appUrl);
