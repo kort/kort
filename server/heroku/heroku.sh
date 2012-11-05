14459c3129bdd91cd1f36b1da4def48a36935d70
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-05 14:44:57 +0100

    Moved env checking so it only gets executing when acutally deploying

--- server/heroku/heroku.sh
+++ server/heroku/heroku.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
-   echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
-   exit 1
-fi
-
 if [[ $DEPLOY == "true" ]] ; then
+    if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
+       echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
+       exit 1
+    fi
     yes | ruby $CI_HOME/server/heroku/heroku_prepare.rb
     bash $CI_HOME/server/heroku/heroku_keys.sh
     bash $CI_HOME/server/heroku/heroku_build.sh
