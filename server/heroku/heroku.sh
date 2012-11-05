d25de07302a35beb468cbf29bb3494dde64e56d2
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-05 08:49:37 +0100

    Moved all heroku stuff in a single script for .travis.yml

---
+++ server/heroku/heroku.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
+   echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
+   exit 1
+fi
+
+yes | ruby $CI_HOME/server/travis_deployer.rb
+bash $CI_HOME/server/heroku/heroku_keys.sh
+bash $CI_HOME/server/heroku/heroku_build.sh
+cd $BUILD_DIR
+bash $CI_HOME/server/heroku/heroku_add.sh >/dev/null
+bash $CI_HOME/server/heroku/heroku_push.sh
