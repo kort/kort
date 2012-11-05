9ee9a67acb9a7d5a965dd29326b648d8fe20530a
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-05 10:19:51 +0100

    started implementing profile page

--- app/controller/Profile.js
+++ app/controller/Profile.js
@@ -7,14 +7,53 @@ Ext.define('Kort.controller.Profile', {
         ],
         refs: {
             mainTabPanel: '#mainTabPanel',
-            profileContainer: '#profileContainer'
+            profileContainer: '#profileContainer',
+            profileContentContainer: '#profileContentContainer',
+            logoutButton: '#logoutButton'
+        },
+        control: {
+            profileContentContainer: {
+                initialize: 'onProfileContentContrainerInitialize'
+            },
+            logoutButton: {
+                tap: 'onLogoutButtonTap'
+            }
         },
         routes: {
             'profile': 'showProfile'
-        }
+        },
+        
+        userStore: null
     },
     
     showProfile: function() {
         this.getMainTabPanel().setActiveItem(this.getProfileContainer());
+    },
+    
+    onProfileContentContrainerInitialize: function() {
+        var store = this.getUserStore(),
+            user;
+            
+        if(!store.isLoaded()) {
+            Ext.defer(this.onProfileContentContrainerInitialize, 500, this);
+        } else {
+            user = this.getUserStore().first();
+            this.getProfileContentContainer().setRecord(user);
+        }
+    },
+    
+    onLogoutButtonTap: function() {
+        var me = this;
+        Ext.Ajax.request({
+            url: './server/webservices/user/logout',
+            success: function(response){
+                // TODO Reset user instance here
+                console.log(response);
+            }
+        });
+    },
+    
+    init: function() {
+        this.setUserStore(Ext.getStore('User'));
     }
 });
