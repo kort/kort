dfc938f62ef794ef03ec3cf3e6c2c39639f0a338
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-05 09:51:37 +0100

    Replaced boolean variable with string

--- server/heroku/heroku_push.sh
+++ server/heroku/heroku_push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if [ $DEPLOY -eq true ] ; then
-    echo "kort" | heroku apps:destroy kort
-    heroku apps:create kort
-    heroku sharing:add juerg.hunziker@gmail.com
+echo "kort" | heroku apps:destroy kort
+heroku apps:create kort
+heroku sharing:add juerg.hunziker@gmail.com
 
-    git push -f heroku master
-fi
+git push -f heroku master
