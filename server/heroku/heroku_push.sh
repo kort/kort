29cb31797fe32796b891a6190226b61166820599
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-05 01:38:37 +0100

    Build for PHP 5.3 and 5.4

--- server/heroku/heroku_push.sh
+++ server/heroku/heroku_push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "kort" | heroku apps:destroy kort
-heroku apps:create kort
-heroku sharing:add juerg.hunziker@gmail.com
+if [ $DEPLOY -eq true ] ; then
+    echo "kort" | heroku apps:destroy kort
+    heroku apps:create kort
+    heroku sharing:add juerg.hunziker@gmail.com
 
-git push -f heroku master
+    git push -f heroku master
+fi
