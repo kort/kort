e9f8cb644a1cd6ced9494214421e93a4e05d9086
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-05 09:09:29 +0100

    Moved environment checking to heroku script

--- server/heroku/heroku.sh
+++ server/heroku/heroku.sh
@@ -4,9 +4,13 @@ if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
    exit 1
 fi
 
-yes | ruby $CI_HOME/server/travis_deployer.rb
-bash $CI_HOME/server/heroku/heroku_keys.sh
-bash $CI_HOME/server/heroku/heroku_build.sh
-cd $BUILD_DIR
-bash $CI_HOME/server/heroku/heroku_add.sh >/dev/null
-bash $CI_HOME/server/heroku/heroku_push.sh
+if [ $DEPLOY ] ; then
+    yes | ruby $CI_HOME/server/travis_deployer.rb
+    bash $CI_HOME/server/heroku/heroku_keys.sh
+    bash $CI_HOME/server/heroku/heroku_build.sh
+    cd $BUILD_DIR
+    bash $CI_HOME/server/heroku/heroku_add.sh >/dev/null
+    bash $CI_HOME/server/heroku/heroku_push.sh
+else
+    echo "Omitting deployment to heroku."
+fi
