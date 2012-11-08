ec653b2f7fb310707c20ec3b7e4fb6db59bed495
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-08 13:46:28 +0100

    Changed index name to not be ignored

--- server/database/keepright_index.sql
+++ server/database/keepright_index.sql
@@ -1,8 +1,8 @@
 ALTER TABLE keepright.errors ADD CONSTRAINT errors_pkey PRIMARY KEY(schema , error_id , object_id );
-CREATE INDEX error_id#idx ON keepright.errors (error_id ASC NULLS LAST);
-CREATE INDEX lat#idx ON keepright.errors (lat ASC NULLS LAST);
-CREATE INDEX lon#idx ON keepright.errors (lon ASC NULLS LAST);
-CREATE INDEX object_type#idx ON keepright.errors (object_type ASC NULLS LAST);
-CREATE INDEX state#idx ON keepright.errors (state ASC NULLS LAST);
-CREATE INDEX error_type#idx ON keepright.errors (error_type ASC NULLS LAST);
-CREATE INDEX error_name#idx ON keepright.errors (error_name ASC NULLS LAST);
+CREATE INDEX error_id_idx ON keepright.errors (error_id ASC NULLS LAST);
+CREATE INDEX lat_idx ON keepright.errors (lat ASC NULLS LAST);
+CREATE INDEX lon_idx ON keepright.errors (lon ASC NULLS LAST);
+CREATE INDEX object_type_idx ON keepright.errors (object_type ASC NULLS LAST);
+CREATE INDEX state_idx ON keepright.errors (state ASC NULLS LAST);
+CREATE INDEX error_type_idx ON keepright.errors (error_type ASC NULLS LAST);
+CREATE INDEX error_name_idx ON keepright.errors (error_name ASC NULLS LAST);
