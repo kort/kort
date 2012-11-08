fb93067799fab2978cc1f73ac9c4215d9b000c4b
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-08 13:29:47 +0100

    Removed schema name from index

--- server/database/keepright_index.sql
+++ server/database/keepright_index.sql
@@ -1,16 +1,8 @@
-ALTER TABLE keepright.errors
-  ADD CONSTRAINT errors_pkey PRIMARY KEY(schema , error_id , object_id );
-CREATE INDEX keepright.error_id#idx
-   ON keepright.errors (error_id ASC NULLS LAST);
-CREATE INDEX keepright.lat#idx
-   ON keepright.errors (lat ASC NULLS LAST);
-CREATE INDEX keepright.lon#idx
-   ON keepright.errors (lon ASC NULLS LAST);
-CREATE INDEX keepright.object_type#idx
-   ON keepright.errors (object_type ASC NULLS LAST);
-CREATE INDEX keepright.state#idx
-   ON keepright.errors (state ASC NULLS LAST);
-CREATE INDEX keepright.error_type#idx
-   ON keepright.errors (error_type ASC NULLS LAST);
-CREATE INDEX keepright.error_name#idx
-   ON keepright.errors (error_name ASC NULLS LAST);
+ALTER TABLE keepright.errors ADD CONSTRAINT errors_pkey PRIMARY KEY(schema , error_id , object_id );
+CREATE INDEX error_id#idx ON keepright.errors (error_id ASC NULLS LAST);
+CREATE INDEX lat#idx ON keepright.errors (lat ASC NULLS LAST);
+CREATE INDEX lon#idx ON keepright.errors (lon ASC NULLS LAST);
+CREATE INDEX object_type#idx ON keepright.errors (object_type ASC NULLS LAST);
+CREATE INDEX state#idx ON keepright.errors (state ASC NULLS LAST);
+CREATE INDEX error_type#idx ON keepright.errors (error_type ASC NULLS LAST);
+CREATE INDEX error_name#idx ON keepright.errors (error_name ASC NULLS LAST);
