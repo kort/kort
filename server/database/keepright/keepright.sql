9f38a2270a1aa5f1ec75e7f36d2d5ce512849f59
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 11:10:49 +0100

    Normalized type out of errors table

--- server/database/keepright/keepright.sql
+++ server/database/keepright/keepright.sql
@@ -4,8 +4,8 @@ create type keepright.osm_type as enum('node','way','relation');
 
 create table keepright.errors (
   schema varchar(6) not null default '',
+  error_id integer not null,
   error_type_id integer not null,
-  error_type integer not null,
   error_name varchar(100) not null,
   object_type osm_type not null,
   object_id bigint not null,
@@ -29,7 +29,7 @@ create table keepright.errors (
 
 create table keepright.error_type (
        error_type_id integer not null,
-       name character varying(20) not null, 
+       type character varying(20) not null, 
        description character varying(100), 
        PRIMARY KEY (error_type_id)
 );
--- server/database/keepright/keepright_cleanup.sql
+++ server/database/keepright/keepright_cleanup.sql
@@ -11,13 +11,12 @@ update keepright.errors set msgid = replace(replace(replace(replace(replace(msgi
 
 
 -- fill data in table error_type
-insert into keepright.error_type (error_type_id, error_type, description) values (90, 'motorway_ref', 'motorways without ref');
-insert into keepright.error_type (error_type_id, error_type, description) values (100, 'religion', 'places of worship without religion');
-insert into keepright.error_type (error_type_id, error_type, description) values (110, 'poi_name', 'point of interest without name');
-insert into keepright.error_type (error_type_id, error_type, description) values (180, 'relation_type', 'relations without type');
-insert into keepright.error_type (error_type_id, error_type, description) values (300, 'missing_maxspeed', 'missing maxspeed');
-insert into keepright.error_type (error_type_id, error_type, description) values (360, 'language_unknown', 'language unknown');
-insert into keepright.error_type (error_type_id, error_type, description) values (390, 'missing_track_type', 'missing tracktype');
+insert into keepright.error_type (error_type_id, type, description) values (90, 'motorway_ref', 'motorways without ref');
+insert into keepright.error_type (error_type_id, type, description) values (100, 'religion', 'places of worship without religion');
+insert into keepright.error_type (error_type_id, type, description) values (110, 'poi_name', 'point of interest without name');
+insert into keepright.error_type (error_type_id, type, description) values (180, 'relation_type', 'relations without type');
+insert into keepright.error_type (error_type_id, type, description) values (300, 'missing_maxspeed', 'missing maxspeed');
+insert into keepright.error_type (error_type_id, type, description) values (360, 'language_unknown', 'language unknown');
+insert into keepright.error_type (error_type_id, type, description) values (390, 'missing_track_type', 'missing tracktype');
 
-ALTER TABLE keepright.errors DROP COLUMN error_type;
 ALTER TABLE keepright.errors DROP COLUMN error_name;
