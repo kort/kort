53c5d021583a03b8d6afe1bcf5fd9bc6f51561d0
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 02:32:08 +0100

    Fixed indenting and spacing (PHP lint)

--- server/php/Helper/PostGisSqlHelper.php
+++ server/php/Helper/PostGisSqlHelper.php
@@ -1,8 +1,10 @@
 <?php
 namespace Helper;
 
-class PostGisSqlHelper {
-   public static function getLatLngGeom($lat, $lng) {
-       return "ST_SetSRID(ST_Point(".$lng.",".$lat."),4326)";
-   }
+class PostGisSqlHelper
+{
+    public static function getLatLngGeom($lat, $lng)
+    {
+        return "ST_SetSRID(ST_Point(".$lng.",".$lat."),4326)";
+    }
 }
