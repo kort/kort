549a78fc7b03d367d29334eb8c2b67de3fef0365
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 17:41:55 +0100

    Adapted name changes in index script

--- server/database/keepright/keepright_index.sql
+++ server/database/keepright/keepright_index.sql
@@ -3,5 +3,6 @@ CREATE INDEX lat_idx ON keepright.errors (lat ASC NULLS LAST);
 CREATE INDEX lon_idx ON keepright.errors (lon ASC NULLS LAST);
 CREATE INDEX object_type_idx ON keepright.errors (object_type ASC NULLS LAST);
 CREATE INDEX state_idx ON keepright.errors (state ASC NULLS LAST);
-CREATE INDEX error_type_idx ON keepright.errors (error_type ASC NULLS LAST);
-CREATE INDEX error_name_idx ON keepright.errors (error_name ASC NULLS LAST);
+CREATE INDEX error_type_id_idx ON keepright.errors (error_type_id ASC NULLS LAST);
+
+CREATE INDEX error_name_idx ON keepright.error_type (type ASC NULLS LAST);
