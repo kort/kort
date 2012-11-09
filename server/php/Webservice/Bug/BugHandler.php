9b72fc431ff23c032289cc5932a524795c352bbe
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 02:28:35 +0100

    Added tests for BugHandler

--- server/php/Webservice/Bug/BugHandler.php
+++ server/php/Webservice/Bug/BugHandler.php
@@ -6,20 +6,11 @@ use Helper\PostGisSqlHelper;
 
 class BugHandler extends AbstractDbHandler
 {
-    public function bugsIdRouteHandler($res)
-    {
-        $handler = $this;
-        return function ($schema, $id) use ($handler, $res) {
-            $res->write($handler->getBugsById($schema, $id));
-        };
-    }
-
-
     public function getBugsById($schema, $id)
     {
         //refactor to generic validate method
         if (is_numeric($schema) && is_numeric($id)) {
-            $result = $this->db->doSelectQuery('schema = \'' . $schema . '\' AND id = ' . $id);
+            $result = $this->db->doSelectQuery('schema = \'' . $schema . '\' and id = ' . $id);
 
             // only return first row (error_id not unique)
             if (count($result) > 0) {
@@ -33,7 +24,6 @@ class BugHandler extends AbstractDbHandler
     {
         $orderBy = "ST_Distance(".PostGisSqlHelper::getLatLngGeom($lat, $lng).",geom)";
         $result = $this->db->doSelectQuery("1=1",$orderBy,$limit);
-
         return json_encode($result);
     }
 }
