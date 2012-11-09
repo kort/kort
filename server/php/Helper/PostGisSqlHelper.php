95ed89e8c331ab6563d8c837da1a72f9e2dec4d3
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 16:18:20 +0100

    Changed geom to SRID 900913 to measure in meters

--- server/php/Helper/PostGisSqlHelper.php
+++ server/php/Helper/PostGisSqlHelper.php
@@ -5,6 +5,6 @@ class PostGisSqlHelper
 {
     public static function getLatLngGeom($lat, $lng)
     {
-        return "ST_SetSRID(ST_Point(".$lng.",".$lat."),4326)";
+        return "ST_Transform(ST_SetSRID(ST_Point(".$lng.",".$lat."),4326),900913)";
     }
 }
