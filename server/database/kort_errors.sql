1e2b9b84933f6072acd48c9c463e2c6e6ce7dd64
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-09 02:00:36 +0100

    Added a view to access errors

---
+++ server/database/kort_errors.sql
@@ -0,0 +1,12 @@
+create or replace view kort.errors as
+select error_id AS id,
+        schema,
+        error_type AS type,
+        object_id AS osm_id,
+        object_type AS osm_type,
+        error_name AS title,
+        msgid AS description,
+        CAST(lat AS NUMERIC)/10000000 AS latitude,
+        CAST(lon AS NUMERIC)/10000000 AS longitude,
+        geom
+from    keepright.errors;
