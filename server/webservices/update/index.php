06fa68dd80ecc467b65fb9d81d98b37f116916e3
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-12 12:01:05 +0100

    Removed unsed dependencies from update service

--- server/webservices/update/index.php
+++ server/webservices/update/index.php
@@ -1,10 +1,8 @@
 <?php
 require_once('../../../lib/Slim-2.1.0/Slim/Slim.php');
-require_once('../../../server/php/ClassLoader.php');
 
 // Load Slim library
 \Slim\Slim::registerAutoloader();
-Kort\ClassLoader::registerAutoLoader();
 
 $app = new \Slim\Slim();
 $app->get(
