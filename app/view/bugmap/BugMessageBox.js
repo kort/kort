8d4f0e3cd42d1cf4d1eebb4a7c6d93d7424eb32c
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-12 20:48:06 +0100

    moved messagebox template to controller

--- app/view/bugmap/BugMessageBox.js
+++ app/view/bugmap/BugMessageBox.js
@@ -5,29 +5,5 @@ Ext.define('Kort.view.bugmap.BugMessageBox', {
     config: {
         cls: 'bugMessageBox',
         zIndex: Kort.util.Config.getOverlayLeafletMapZIndex()
-    },
-    
-    initialize: function () {
-        this.callParent(arguments);
-
-        var messageBoxContent = {
-            xtype: 'component',
-            cls: 'x-msgbox-text',
-            record: this.getRecord(),
-            tpl:    new Ext.Template(
-                        '<div class="confirm-content">',
-                            '<div class="description">',
-                                '<div class="image">',
-                                    //'<img class="bugtype-image" src="resources/images/bugtypes/{type}.png" />',
-                                '</div>',
-                                '<div class="content">',
-                                    '<p>{description}</p>',
-                                '</div>',
-                            '</div>',
-                        '</div>'
-                    )
-        };
-        
-        this.add([messageBoxContent]);
     }
 });
