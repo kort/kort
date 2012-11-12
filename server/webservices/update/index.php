8865a26296ff29e67ce9d37439218a0dd3abf226
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-12 11:36:23 +0100

    Added update service for db server

---
+++ server/webservices/update/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../../../lib/Slim-2.1.0/Slim/Slim.php');
+require_once('../../../server/php/ClassLoader.php');
+
+// Load Slim library
+\Slim\Slim::registerAutoloader();
+Kort\ClassLoader::registerAutoLoader();
+
+
+$app = new \Slim\Slim();
+
+$app->get(
+    '/git',
+    function () use ($app) {
+        system('cd webservices && git pull');
+    }
+);
+
+// start Slim app
+$app->run();
