578d0b5bc909b2bc9bf2b97d9b634fc825e290a8
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-12 09:12:00 +0100

    Translated error type to German

--- server/database/keepright/keepright_cleanup.sql
+++ server/database/keepright/keepright_cleanup.sql
@@ -13,12 +13,13 @@ update keepright.errors set msgid = replace(replace(replace(replace(replace(msgi
 
 
 -- fill data in table error_type
-insert into keepright.error_type (error_type_id, type, description) values (90, 'motorway_ref', 'motorways without ref');
-insert into keepright.error_type (error_type_id, type, description) values (100, 'religion', 'places of worship without religion');
-insert into keepright.error_type (error_type_id, type, description) values (110, 'poi_name', 'point of interest without name');
-insert into keepright.error_type (error_type_id, type, description) values (180, 'relation_type', 'relations without type');
-insert into keepright.error_type (error_type_id, type, description) values (300, 'missing_maxspeed', 'missing maxspeed');
-insert into keepright.error_type (error_type_id, type, description) values (360, 'language_unknown', 'language unknown');
-insert into keepright.error_type (error_type_id, type, description) values (390, 'missing_track_type', 'missing tracktype');
+insert into keepright.error_type (error_type_id, type, description) values (90, 'motorway_ref', 'Autobahn ohne Bezeichner');
+insert into keepright.error_type (error_type_id, type, description) values (100, 'religion', 'Kultst?tte/Kirche ohne Religion');
+insert into keepright.error_type (error_type_id, type, description) values (110, 'poi_name', 'Objekt ohne Namen');
+insert into keepright.error_type (error_type_id, type, description) values (180, 'relation_type', 'Beziehung ohne Typ');
+insert into keepright.error_type (error_type_id, type, description) values (300, 'missing_maxspeed', 'Fehlende Geschwindigkeitsbegrenzung');
+insert into keepright.error_type (error_type_id, type, description) values (360, 'language_unknown', 'Sprache unbekannt');
+insert into keepright.error_type (error_type_id, type, description) values (390, 'missing_track_type', 'Strassentyp unbekannt');
+insert into keepright.error_type (error_type_id, type, description) values (400, 'missing_tags', 'Fehlende Information');
 
 ALTER TABLE keepright.errors DROP COLUMN error_name;
