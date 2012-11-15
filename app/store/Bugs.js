d7ba37fcf5b5c7b7bc8e6287b863a70a7f5d3b6d
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-15 16:30:11 +0100

    Revert "Removed radius parameter and used new distance operator of PostGIS for nearest-neighbour query"
    This reverts commit ffd043178f659e615c7e2603ff492219e1325aa8.

--- app/store/Bugs.js
+++ app/store/Bugs.js
@@ -1,12 +1,15 @@
 Ext.define('Kort.store.Bugs', {
     extend: 'Ext.data.Store',
-
+	
 	config: {
 		model: 'Kort.model.Bug',
-
+		
 		proxy: {
 			type: "ajax",
             url : "./resources/stores/bugs.json",
+            extraParams: {
+                'radius': Kort.util.Config.getBugs().radius
+            },
             reader: {
                 type: "json"
             }
