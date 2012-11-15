ffd043178f659e615c7e2603ff492219e1325aa8
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-15 16:05:18 +0100

    Removed radius parameter and used new distance operator of PostGIS for nearest-neighbour query

--- server/webservices/db/index.php
+++ server/webservices/db/index.php
@@ -18,9 +18,8 @@ $app->get(
     '/position/:lat,:lng',
     function ($lat, $lng) use ($bugHandler, $res) {
         $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
-        $radius = (isset($_GET['radius'])) ? $_GET['radius'] : 5000;
 
-        $res->write($bugHandler->getBugsByOwnPosition($lat, $lng, $limit, $radius));
+        $res->write($bugHandler->getBugsByOwnPosition($lat, $lng, $limit));
     }
 );
 $app->get(
