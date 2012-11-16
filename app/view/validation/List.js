74c95ca33368fae3edb81d7334af95d51fbc8ade
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-16 13:32:47 +0100

    styled validation list

--- app/view/validation/List.js
+++ app/view/validation/List.js
@@ -1,6 +1,9 @@
 Ext.define('Kort.view.validation.List', {
 	extend: 'Ext.List',
 	alias: 'widget.validationlist',
+    requires: [
+        'Kort.plugin.PullRefresh'
+    ],
     
 	config: {
 		layout: 'fit',
@@ -8,6 +11,30 @@ Ext.define('Kort.view.validation.List', {
         grouped: true,
         loadingText: Ext.i18n.Bundle.message('validation.loadmask.message'),
         emptyText: Ext.i18n.Bundle.message('validation.emptytext'),
-        itemTpl: '<div>{title} / {formattedDistance}</div>'
+        itemTpl:    '<div class="validation-item">' +
+                        '<div class="image">' +
+                            '<img class="bugtype-image" src="./resources/images/marker_icons/{type}.png" />' +
+                        '</div>' +
+                        '<div class="content">' +
+                            '<div class="title">{title}</div>' +
+                            '<div class="ratings">' +
+                                '<span class="upratings">' +
+                                    '+{upratings}' +
+                                    '<img class="thumb" src="./resources/images/validation/thumbs-up.png" />' +
+                                '</span>' +
+                                '<span class="downratings">' +
+                                    '-{downratings}' +
+                                    '<img class="thumb" src="./resources/images/validation/thumbs-down.png" />' +
+                                '</span>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="distance">{formattedDistance}</div>' +
+                    '</div>',
+        
+        plugins: [
+            {
+                xclass: 'Kort.plugin.PullRefresh'
+            }
+        ]
 	}
 });
