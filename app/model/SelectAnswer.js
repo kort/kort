bdd1851427530ca98a7d21bd7dc9f19ff2bab688
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-19 15:45:12 +0100

    renamed select answers store

---
+++ app/model/SelectAnswer.js
@@ -0,0 +1,14 @@
+Ext.define('Kort.model.SelectAnswer', {
+    extend: 'Ext.data.Model',
+    config: {
+		idProperty: 'id',
+		
+        fields: [
+			{ name: 'id', type: 'auto' },
+			{ name: 'value', type: 'string' },
+			{ name: 'title', type: 'string' },
+			{ name: 'type', type: 'string' },
+			{ name: 'sorting', type: 'integer' }
+        ]
+    }
+});
