a0c19702e62267c432bdc40277208408104b8ba1
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-19 10:00:12 +0100

    created fresh testing build

--- build/Kort/testing/server/php/Helper/PostGisSqlHelper.php
+++ build/Kort/testing/server/php/Helper/PostGisSqlHelper.php
@@ -5,6 +5,6 @@ class PostGisSqlHelper
 {
     public static function getLatLngGeom($lat, $lng)
     {
-        return "ST_Transform(ST_SetSRID(ST_Point(".$lng.",".$lat."),4326),900913)";
+        return "ST_SetSRID(ST_Point(".$lng.",".$lat."),4326)";
     }
 }
