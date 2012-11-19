f9b254db303a43bf9ec79631303759562f6dbe30
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-19 10:00:44 +0100

    ignore jshint warnings in overridden sencha methods

--- app/plugin/PullRefresh.js
+++ app/plugin/PullRefresh.js
@@ -39,6 +39,7 @@ Ext.define('Kort.plugin.PullRefresh', {
 	},
     
     initScrollable: function() {
+        /* jshint maxcomplexity:10 */
         var me = this,
             list = me.getList(),
             store = list.getStore(),
@@ -95,6 +96,7 @@ Ext.define('Kort.plugin.PullRefresh', {
     },
     
     onBounceTop: function(y) {
+        /* jshint maxcomplexity:10 */
         var me = this,
             pullHeight = me.pullHeight,
             list = me.getList(),
