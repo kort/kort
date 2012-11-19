6096d1bb940a2549798b9a6b3f6930bc492be890
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-19 22:21:17 +0100

    Added tests for bug webservice

--- test/client/helper.js
+++ test/client/helper.js
@@ -21,3 +21,31 @@ if (params.debug !== undefined) {
 		console.log('####');
 	};
 }
+
+/*inspired by http://www.beletsky.net/2010/12/testing-rest-services-with-javascript.html */
+function api_test(url, type, data, callback, raw) {
+    raw = raw || false;
+
+    $.ajax({
+        url: url,
+        type: type,
+        processData: false,
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        async: false,
+        complete: function (result) {
+            if (raw) {
+                callback(result);
+            } else {
+                if (result.status == 0) {
+                    ok(false, '0 status - browser could be on offline mode');
+                } else if (result.status == 404) {
+                    ok(false, '404 error');
+                } else {
+                    callback($.parseJSON(result.responseText));
+                }
+            }
+        }
+    });
+}
