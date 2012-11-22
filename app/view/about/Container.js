df80f4c0460bc1ea2f10b3bcbc0f5631b42b7392
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-11-22 09:33:35 +0100

    styling of highscore, profile and about page

--- app/view/about/Container.js
+++ app/view/about/Container.js
@@ -10,7 +10,7 @@ Ext.define('Kort.view.about.Container', {
 		url: 'about',
 		id: 'aboutContainer',
 		iconCls: 'info',
-		layout: 'fit',
+		layout: 'vbox',
         scrollable: true,
 		items: [
 			{
@@ -24,9 +24,14 @@ Ext.define('Kort.view.about.Container', {
                             '<div class="logo">' +
                                 '<img src="./resources/images/kort-logo.png" />' +
                             '</div>' +
-                            '<dl>' +
+                            '<dl class="kort-definitionlist info">' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.version.title') + '</dt>' +
                                 '<dd>' + Kort.util.Config.getVersion() + '</dd>' +
+                                '<dt>' + Ext.i18n.Bundle.message('about.authors.title') + '</dt>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.authors.author1') + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.authors.author2') + '</dd>' +
+                                '<dt>' + Ext.i18n.Bundle.message('about.legal.title') + '</dt>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.legal.message') + '</dd>' +
                             '</dl>' +
                         '</div>'
 			}
