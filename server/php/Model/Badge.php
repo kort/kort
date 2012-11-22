32d59e2bdc1df3ffebe46e4c9d31171ecb8eac07
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-22 12:20:15 +0100

    Return reward after providing a fix

---
+++ server/php/Model/Badge.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Model;
+
+class Badge {
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public static function getValueFn()
+    {
+        return function($badge) { return $badge->getName(); };
+    }
+}
