f1ff11fcf917898e1ca2fa7fa18ef2186448ad51
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-22 11:52:30 +0100

    Updated testing build

---
+++ build/Kort/testing/server/php/Webservice/Answer/Test/TestAnswerHandler.php
@@ -0,0 +1,51 @@
+<?php
+namespace Webservice\Answer\Test;
+
+use TestHelper\AbstractKortUnitTestCase;
+use Webservice\DbProxy;
+use Webservice\Answer\AnswerHandler;
+use \Mockery as M;
+
+class TestAnswerHandler extends AbstractKortUnitTestCase
+{
+    public function __construct()
+    {
+        parent::__construct("kort - TestAnswerHandler");
+    }
+
+    public function setUp()
+    {
+        $this->mockProxy = M::mock(
+            'DbProxy',
+            array(
+                "setLimit" => null,
+                "setOrderBy" => null,
+                "setWhere" => null
+            )
+        );
+        $this->handler = new AnswerHandler();
+        $this->handler->setDbProxy($this->mockProxy);
+    }
+
+    public function tearDown()
+    {
+        M::close();
+    }
+
+    public function testConstruct()
+    {
+        $this->assertIsA($this->handler, "Webservice\\Answer\\AnswerHandler");
+    }
+
+    public function testGetSpecificAnswers()
+    {
+        $this->mockProxy->shouldReceive('getFromDb')->andReturn("{\"test\":\"value\"}");
+        $this->assertEqual("{\"test\":\"value\"}", $this->handler->getSpecificAnswers("some_type", 1));
+    }
+
+    public function testGetSpecificAnswersEmpty()
+    {
+        $this->mockProxy->shouldReceive('getFromDb')->andReturn("");
+        $this->assertEqual("", $this->handler->getSpecificAnswers("some_type", 1));
+    }
+}
