60c2d3c37b04971590c3b8880d8c6ed80005ec35
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-22 10:24:21 +0100

    Added tests for DbHandler

--- server/php/Webservice/Database/Test/TestDbHandler.php
+++ server/php/Webservice/Database/Test/TestDbHandler.php
@@ -1,22 +1,27 @@
 <?php
-namespace Webservice\Bug\Test;
+namespace Webservice\Database\Test;
 
 use TestHelper\AbstractKortUnitTestCase;
-use Webservice\Bug\BugHandler;
+use Webservice\Database\DbHandler;
 use \Mockery as M;
 
-class TestBugHandler extends AbstractKortUnitTestCase
+class TestDbHandler extends AbstractKortUnitTestCase
 {
     public function __construct()
     {
-        parent::__construct("kort - TestBugHandler");
+        parent::__construct("kort - TestDbHandler");
     }
 
     public function setUp()
     {
-        $this->mockProxy = M::mock('DbProxy');
-        $this->handler = new BugHandler();
-        $this->handler->setDbProxy($this->mockProxy);
+        $this->mockDb = M::mock('PsqlConnection');
+        $this->handler = new DbHandler($this->mockDb);
+
+        $this->fields = array("id", "title");
+        $this->table = "schema.testtable";
+        $this->where = "id = 7";
+        $this->orderBy = "title desc";
+        $this->limit = 10;
     }
 
     public function tearDown()
@@ -26,14 +31,37 @@ class TestBugHandler extends AbstractKortUnitTestCase
 
     public function testConstruct()
     {
-        $this->assertIsA($this->handler, "Webservice\\Bug\\BugHandler");
+        $this->assertIsA($this->handler, "Webservice\\Database\\DbHandler");
     }
 
-    public function testGetBugsByOwnPosition()
+    public function testDoSelect()
     {
-        $this->mockProxy->shouldReceive('setOrderBy')/*->with('/4000/')*/;
-        $this->mockProxy->shouldReceive('setLimit')->with(50);
-        $this->mockProxy->shouldReceive('getFromDb')->andReturn("{\"test\":\"value\"}");
-        $this->assertEqual("{\"test\":\"value\"}", $this->handler->getBugsByOwnPosition(47, 8, 50, 4000));
+        $this->mockDb->shouldReceive('doSelectQuery')->andReturn(array("id" => 7, "title" => "A test title"));
+        $resultJson = $this->handler->doSelect(
+            $this->fields,
+            $this->table,
+            $this->where,
+            $this->orderBy,
+            $this->limit
+        );
+
+        $result = json_decode($resultJson, true);
+        $this->assertEqual(json_last_error(), JSON_ERROR_NONE, "Result should be valid JSON");
+        $this->assertEqual(7, $result['id'], "ID should equal the input");
+        $this->assertEqual("A test title", $result['title'], "Title should equal the input");
+    }
+
+    public function testDoSelectEmpty()
+    {
+        $this->mockDb->shouldReceive('doSelectQuery')->andReturn(array());
+        $resultJson = $this->handler->doSelect(
+            $this->fields,
+            $this->table,
+            $this->where,
+            $this->orderBy,
+            $this->limit
+        );
+
+        $this->assertEqual("", $resultJson, "If the return is empty no JSON should be returned, empty string instead.");
     }
 }
