853a5510b831f363dbe39d243b5eb40ab53387b4
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-23 10:08:15 +0100

    More verbose output for setup_postgis.sh

--- server/database/setup_postgis.sh
+++ server/database/setup_postgis.sh
@@ -37,21 +37,28 @@ fi
 # See here to get latest release: http://linfiniti.com/2012/05/installing-postgis-2-0-on-ubuntu/
 
 # uninstall extension (in case it was already installed)
+echo "Uninstall previous installation of PostGIS"
 psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.1/uninstall_postgis.sql 
 
 # install extension
+echo "Install PostGIS"
 psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.1/postgis.sql
+echo "Install spatial_ref_sys"
 psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.1/spatial_ref_sys.sql
 
+echo "Grants for PostGIS"
 psql -d $DB_NAME -c "GRANT ALL ON geometry_columns TO PUBLIC;"
 psql -d $DB_NAME -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 psql -d $DB_NAME -c "GRANT ALL ON geography_columns TO PUBLIC;"
 
 # add geometry to table
+echo "Add geometry column to $SCHEMA_NAME.$TABLE_NAME"
 psql -d $DB_NAME -c "select AddGeometryColumn ('$SCHEMA_NAME','$TABLE_NAME','geom', 4326,'POINT',2);"
 
 # update table
+echo "Generate geometry objects based on lat/lng values"
 psql -d $DB_NAME -c "update $SCHEMA_NAME.$TABLE_NAME set geom = ST_SetSRID(ST_Point(lon/10000000.0,lat/10000000.0),4326);"
 
 # create spatial index
+echo "Create spatial index"
 psql -d $DB_NAME -c "create index geom_idx on $SCHEMA_NAME.$TABLE_NAME using gist(geom);"
