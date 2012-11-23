ba2123006821a08d37f7476f94f63e592b0e72f3
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-23 12:26:38 +0100

    Extracted UserGetHandler to handle the GET requests of the user webservice

--- server/php/Webservice/User/UserHandler.php
+++ server/php/Webservice/User/UserHandler.php
@@ -5,27 +5,14 @@ use Webservice\DbProxyHandler;
 
 class UserHandler extends DbProxyHandler
 {
-    protected $userData = array();
-
     protected $table = 'kort.user';
     protected $fields = array(
-        'id',
+        'user_id',
         'name',
         'email',
         'username',
-        'picUrl',
         'token'
     );
-
-    public function getUser($id)
-    {
-        $this->getDbProxy()->setWhere("id = ". $id);
-        $userData = json_decode($this->getDbProxy()->getFromDb(), true);
-        $userData['picUrl'] = $this->getGravatarUrl( $userData['email']);
-        $userData['loggedIn'] = isset($_SESSION['token']);
-
-        return json_encode($userData);
-    }
 
     public function updateUser($id, $data)
     {
@@ -35,60 +22,5 @@ class UserHandler extends DbProxyHandler
     public function insertUser($data)
     {
         // TODO implement insertUser
-    }
-
-    public function getUserBadges($id)
-    {
-        // TODO implement badges query
-        $badges = array(
-            array(
-                'id' => 1,
-                'name' => 'highscore_place_1',
-                'won' => false
-            ),
-            array(
-                'id' => 2,
-                'name' => 'highscore_place_2',
-                'won' => false
-            ),
-            array(
-                'id' => 3,
-                'name' => 'highscore_place_3',
-                'won' => true
-            ),
-            array(
-                'id' => 4,
-                'name' => 'fix_count_100',
-                'won' => false
-            ),
-            array(
-                'id' => 5,
-                'name' => 'fix_count_50',
-                'won' => true
-            ),
-            array(
-                'id' => 6,
-                'name' => 'fix_count_10',
-                'won' => true
-            )
-        );
-        return \json_encode($badges);
-    }
-
-    /**
-    * Get either a Gravatar URL or complete image tag for a specified email address.
-    *
-    * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
-    * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
-    * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
-    * @return String containing the URL
-    * @source http://gravatar.com/site/implement/images/php/
-    */
-    private function getGravatarUrl ($s = 200, $d = 'mm', $r = 'r')
-    {
-        $url = 'http://www.gravatar.com/avatar/';
-        $url .= \md5(\strtolower(\trim($this->userData['email'])));
-        $url .= "?s=$s&d=$d&r=$r";
-        return $url;
     }
 }
