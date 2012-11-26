6892918cda4bc7f11f36ca789b5968e60bea9d8a
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-26 20:28:40 +0100

    Replaced table and fields attribute with getter functions

--- server/php/Webservice/Fix/FixHandler.php
+++ server/php/Webservice/Fix/FixHandler.php
@@ -7,24 +7,24 @@ use Model\Reward;
 
 class FixHandler extends DbProxyHandler
 {
-    protected $table = 'kort.fix';
-    protected $fields = array(
-        'id',
-        'create_date',
-        'error_id',
-        'message'
-    );
+    protected function getTable()
+    {
+        return 'kort.fix';
+    }
+    
+    protected function getFields()
+    {
+        return array('error_id', 'message');
+    }
 
     public function insertFix($data)
     {
-        $data['id'] = "nextval('kort.fix_id')";
-        $data['create_date'] = "now()";
-        $this->getDbProxy()->insert($data);
+        $return = $this->getDbProxy()->insert($data);
 
         $koinCount = 100;
         $firstBadge = new Badge("highscore_place_1");
         $voteBadge = new Badge("vote_count_10");
         $reward = new Reward($koinCount, array($firstBadge, $voteBadge));
-        return $reward->toJson();
+        return $return; //$reward->toJson();
     }
 }
