75335f7672069d6208e0dc094029701472b65159
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-26 20:29:57 +0100

    Added default values for generated ids and creation date

--- server/database/kort/kort.sql
+++ server/database/kort/kort.sql
@@ -6,19 +6,19 @@ create sequence kort.validation_id;
 create sequence kort.user_id;
 
 create table kort.fix (
-    fix_id integer primary key,
+    fix_id integer primary key default nextval('kort.fix_id'),
     user_id integer,
-    create_date timestamp not null,
+    create_date timestamp not null default now(),
     error_id integer not null,
     message text
 );
 
 create table kort.user (
-    user_id integer primary key,
+    user_id integer primary key default nextval('kort.user_id'),
     name varchar(100) not null,
     username varchar(100),
     email varchar(100),
-    koin_count integer not null,
+    koin_count integer not null default 0,
     token varchar(255),
     oauth_provider varchar(100)
 );
@@ -48,7 +48,7 @@ create table kort.answer (
 );
 
 create table kort.validation (
-    validation_id integer primary key,
+    validation_id integer primary key default nextval('kort.validation_id'),
     user_id integer,
     fix_id integer,
     valid boolean
