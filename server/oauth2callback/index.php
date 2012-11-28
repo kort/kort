3fd30ade5f36094472f56992146d6f21fb36b02f
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-28 22:48:12 +0100

    Persist the user using the infomation received from OAuth

--- server/oauth2callback/index.php
+++ server/oauth2callback/index.php
@@ -1,17 +1,35 @@
 <?php
 require_once "../../lib/google-api-php-client/src/Google_Client.php";
 require_once '../../lib/google-api-php-client/src/contrib/Google_Oauth2Service.php';
+require_once('../php/ClassLoader.php');
 
+Kort\ClassLoader::registerAutoLoader();
 $client = new Google_Client();
 $oauth2 = new Google_Oauth2Service($client);
 
+//refresh user info
 if (isset($_GET['code'])) {
     $client->authenticate($_GET['code']);
     session_start();
-    $_SESSION['token'] = $client->getAccessToken();
+    $token = $client->getAccessToken();
+
     $user = $oauth2->userinfo->get();
-    $_SESSION['email'] = filter_var($user['email'], FILTER_SANITIZE_EMAIL);
-    $_SESSION['name'] = $user['name'];
+    $fields = array('name', 'email', 'oauth_provider');
+    $table = "kort.user";
+    $user['oauth_provider'] = "Google";
+
+    $authObj = json_decode($token);
+    if (isset($authObj->refresh_token)) {
+           $user['token'] = $authObj->refresh_token;
+           $fields[] = "token";
+    }
+
+    //save user data in database
+    $dbProxy = new Webservice\DbProxy($table, $fields);
+    $dbProxy->setReturnFields(array('user_id'));
+    $insertedUser = json_decode($dbProxy->insert($user), true);
+
+     $_SESSION['user_id'] = $insertedUser['user_id'];
 }
 
 $appUrl = 'http://' . $_SERVER['HTTP_HOST'];
