332c6ac8b2468fbb77a52d93e78f57e8c08651c3
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-30 09:46:55 +0100

    Added schema name to owner transfer

--- server/database/setup_kort_db.sh
+++ server/database/setup_kort_db.sh
@@ -47,9 +47,9 @@ echo "Creating views"
 psql -d $DB_NAME -f $DIR/kort/kort_views.sql
 
 echo "Transfer ownership of all objects to $DB_OWNER"
-for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter table $tbl owner to $DB_OWNER" $DB_NAME ; done
-for tbl in `psql -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter table $tbl owner to $DB_OWNER" $DB_NAME ; done
-for tbl in `psql -qAt -c "select table_name from information_schema.views where table_schema = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter table $tbl owner to $DB_OWNER" $DB_NAME ; done
+for tbl in `psql -qAt -c "select schemaname || '.' || tablename from pg_tables where schemaname = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter table $tbl owner to $DB_OWNER" $DB_NAME ; done
+for seq in `psql -qAt -c "select sequence_schema || '.' || sequence_name from information_schema.sequences where sequence_schema = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter $seq owner to $DB_OWNER" $DB_NAME ; done
+for view in `psql -qAt -c "select table_schema || '.' || table_name from information_schema.views where table_schema = '$DB_SCHEMA';" $DB_NAME` ; do  psql -c "alter table $view owner to $DB_OWNER" $DB_NAME ; done
 
 echo "Inserting data"
 psql -d $DB_NAME -f $DIR/kort/kort_data.sql
