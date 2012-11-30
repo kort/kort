e1e267400537e27c1c85d223f470ef67eb6052c4
Stefan Oderbolz <oderbolz@gmail.com>
2012-11-30 11:02:09 +0100

    Secured the post webservices validation/vote and bug/fix to check for missing data and authentication

--- server/webservices/bug/index.php
+++ server/webservices/bug/index.php
@@ -26,9 +26,17 @@ $app->post(
     '/fix',
     function () use ($fixHandler, $app) {
         $data = json_decode($app->request()->getBody(), true);
-        if (json_last_error() !== JSON_ERROR_NONE) {
+
+        if (!isset($_SESSION) || $data['user_id'] != $_SESSION['user_id']) {
+            $app->response()->status(403);
+            $app->response()->write("Wrong user_id");
+            return;
+        }
+
+        if (empty($data) || json_last_error() !== JSON_ERROR_NONE) {
             $app->response()->status(400);
             $app->response()->write("Invalid JSON given!");
+            return;
         }
         $result = $fixHandler->insertFix($data);
         if (!$result) {
