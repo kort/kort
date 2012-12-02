731aecd94faf9e1a474c973124c522439d4ae466
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-12-02 20:54:40 +0100

    implemented send masks for fix and vote

--- app/controller/Vote.js
+++ app/controller/Vote.js
@@ -51,13 +51,16 @@ Ext.define('Kort.controller.Vote', {
             detailTabPanel = this.getDetailTabPanel(),
             userId = Kort.user.get('id'),
             vote;
-
+            
+        me.showSendMask();
         vote = Ext.create('Kort.model.Vote', { fix_id: detailTabPanel.getRecord().get('id'), user_id: userId, valid: valid });
         vote.save({
             success: function(records, operation) {
+                me.hideSendMask();
                 me.voteSuccessfulSubmittedHandler(operation.getResponse().responseText);
             },
             failure: function() {
+                me.hideSendMask();
                 var messageBox = Ext.create('Kort.view.NotificationMessageBox');
                 messageBox.alert(Ext.i18n.Bundle.message('vote.alert.submit.failure.title'), Ext.i18n.Bundle.message('vote.alert.submit.failure.message'), Ext.emptyFn);
             }
@@ -73,6 +76,18 @@ Ext.define('Kort.controller.Vote', {
         this.showRewardMessageBox(reward);
         // remove detail panel
         this.getValidationNavigationView().pop();
+    },
+    
+    showSendMask: function() {
+        this.getValidationNavigationView().setMasked({
+            xtype: 'loadmask',
+            message: Ext.i18n.Bundle.message('vote.sendmask.message'),
+            zIndex: Kort.util.Config.getZIndex().overlayLeafletMap
+        });
+    },
+    
+    hideSendMask: function() {
+        this.getValidationNavigationView().setMasked(false);
     },
     
 	showRewardMessageBox: function(reward) {
