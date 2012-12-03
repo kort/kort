6a93d45add57505f2520704d85217498fd381a6f
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-03 07:31:16 +0100

    Added translation to DbProxyHandler and implemented it in BugHandler

--- server/php/Webservice/DbProxyHandler.php
+++ server/php/Webservice/DbProxyHandler.php
@@ -1,18 +1,27 @@
 <?php
 namespace Webservice;
 
+use Helper\LocaleHelper;
+
 abstract class DbProxyHandler
 {
     private $dbProxy;
+    private $reader;
 
     public function __construct()
     {
         $this->dbProxy = new DbProxy($this->getTable(), $this->getFields());
+        $this->reader = new LocaleHelper("de_CH");
     }
 
     protected function getDbProxy()
     {
         return $this->dbProxy;
+    }
+
+    protected function translate($key)
+    {
+        return $this->reader->getValue($key);
     }
 
     //used for unit testing
