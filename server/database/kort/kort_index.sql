773e2e6d00befb1a6f0aa98cbbd43a4abb3bfd7a
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-04 01:46:47 +0100

    Removed rare languages to reduce amount of options in UI

--- server/database/kort/kort_index.sql
+++ server/database/kort/kort_index.sql
@@ -1,2 +1,3 @@
 CREATE UNIQUE INDEX oauth_user_idx ON kort.user (oauth_provider, oauth_user_id);
 CREATE UNIQUE INDEX secret_idx ON kort.user (secret);
+CREATE INDEX answer_type_idx ON kort.answer (type ASC NULLS LAST);
