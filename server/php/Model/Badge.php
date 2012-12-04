6f7424c69cbbcb2f7bd4722ccba7f65dabeee715
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-05 00:25:06 +0100

    Added initial phpDocs

--- server/php/Model/Badge.php
+++ server/php/Model/Badge.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * kort - Badge class
+ */
 
 namespace Model;
 
+/**
+ * The Badge class represents an achievment, that user can win.
+ *
+ * It is part of the reward that user gets for playing kort.
+ *
+ * @see Model\Reward
+ */
 class Badge
 {
+    /** The name of the badge */
     protected $name;
 
     public static function findById($id)
@@ -28,22 +39,43 @@ class Badge
             9 => 'vote_count_10'
         );
     }
-
+    
+    /**
+    * Creates a new instance of Badge
+    * @param the name of the badge
+    * @return a new instance of Badge
+    */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    /**
+    * Returns the badge's name
+    * @return string containing the badge's name
+    */
     public function getName()
     {
         return $this->name;
     }
 
-
-    public static function getValueFn()
+    /**
+    * Returns an array of values of the given badges
+    * @static
+    * @param $badges whose values should be returned
+    * @return an array of values of the given badges
+    */
+    public static function getValues($badges)
     {
-        return function ($badge) {
-            return array("name" => $badge->getName());
-        };
+        return array_map("Badges::getValue", $badges);
+    }
+
+    /**
+    * Returns an array of properties representing the 'value' of a badge
+    * @param $badge whose value should be returned
+    * @return an array of values of the given badges
+    */
+    private static function getValue($badge) {
+        return array("name" => $badge->getName());
     }
 }
