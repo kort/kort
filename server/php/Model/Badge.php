5b8fb54efacb7c8c4960952d05105b95890a578d
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-07 17:35:27 +0100

    Check for won badges and return them to client

--- server/php/Model/Badge.php
+++ server/php/Model/Badge.php
@@ -6,6 +6,29 @@ class Badge
 {
     protected $name;
 
+    public static function findById($id)
+    {
+        $names = self::getNames();
+        if (array_key_exists($id, $names)) {
+            return new Badge($names[$id]);
+        }
+        return null;
+    }
+
+    protected static function getNames() {
+        return array(
+            1 => 'highscore_place_1',
+            2 => 'highscore_place_2',
+            3 => 'highscore_place_3',
+            4 => 'fix_count_100',
+            5 => 'fix_count_50',
+            6 => 'fix_count_10',
+            7 => 'vote_count_1000',
+            8 => 'vote_count_100',
+            9 => 'vote_count_10'
+        );
+    }
+
     public function __construct($name)
     {
         $this->name = $name;
