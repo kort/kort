6fc81d5e2b24e97a3775eb6af6e0cbca417fc95c
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-08 14:10:38 +0100

    New testing build

--- build/Kort/testing/server/php/Model/Badge.php
+++ build/Kort/testing/server/php/Model/Badge.php
@@ -1,26 +1,81 @@
 <?php
+/**
+ * kort - Badge class
+ */
 
 namespace Model;
 
+/**
+ * The Badge class represents an achievment, that user can win.
+ *
+ * It is part of the reward that user gets for playing kort.
+ *
+ * @see Model\Reward
+ */
 class Badge
 {
+    /** The name of the badge */
     protected $name;
 
+    public static function findById($id)
+    {
+        $names = self::getNames();
+        if (array_key_exists($id, $names)) {
+            return new Badge($names[$id]);
+        }
+        return null;
+    }
+
+    protected static function getNames() {
+        return array(
+            1 => 'highscore_place_1',
+            2 => 'highscore_place_2',
+            3 => 'highscore_place_3',
+            4 => 'fix_count_100',
+            5 => 'fix_count_50',
+            6 => 'fix_count_10',
+            7 => 'vote_count_1000',
+            8 => 'vote_count_100',
+            9 => 'vote_count_10'
+        );
+    }
+    
+    /**
+    * Creates a new instance of Badge
+    * @param the name of the badge
+    * @return a new instance of Badge
+    */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    /**
+    * Returns the badge's name
+    * @return string containing the badge's name
+    */
     public function getName()
     {
         return $this->name;
     }
 
-
-    public static function getValueFn()
+    /**
+    * Returns an array of values of the given badges
+    * @static
+    * @param $badges whose values should be returned
+    * @return an array of values of the given badges
+    */
+    public static function getValues($badges)
     {
-        return function ($badge) {
-            return array("name" => $badge->getName());
-        };
+        return array_map("Badges::getValue", $badges);
+    }
+
+    /**
+    * Returns an array of properties representing the 'value' of a badge
+    * @param $badge whose value should be returned
+    * @return an array of values of the given badges
+    */
+    private static function getValue($badge) {
+        return array("name" => $badge->getName());
     }
 }
--- build/Kort/testing/server/php/Webservice/Vote/VoteHandler.php
+++ build/Kort/testing/server/php/Webservice/Vote/VoteHandler.php
@@ -21,7 +21,7 @@ class VoteHandler extends DbProxyHandler
     {
         $voteKoinCount = "(select vote_koin_count from kort.validations where id = " . $data['fix_id'] . ")";
 
-        $params = array($data);
+        $params = array();
         $params['table'] = "kort.user";
         $params['fields'] = array("koin_count");
         $params['data'] = array(
@@ -40,15 +40,73 @@ class VoteHandler extends DbProxyHandler
 
     }
 
-    protected function insertVoteParams($data) {
-        $params = array();
-        $params['table'] = $this->getTable();
-        $params['fields'] = $this->getFields();
-        $params['returnFields'] = $this->getFields();
-        $params['data'] = $data;
+    protected function getHighscoreBadgesParams($data)
+    {
+        $sql  = "insert into kort.user_badge (user_id, badge_id) ";
+        $sql .= "select h.user_id, b.badge_id ";
+        $sql .= "from   kort.highscore h ";
+        $sql .= "inner join kort.badge b on b.name = 'highscore_place_' || h.ranking ";
+        $sql .= "where  h.user_id = " . $data['user_id'] . " ";
+        $sql .= "and not exists (select 1 from kort.user_badge ub where ub.user_id = h.user_id and ub.badge_id = b.badge_id)";
+        $sql .= "returning badge_id";
 
-        $params['return'] = false;
-        $params['type'] = "INSERT";
+        $params = array();
+        $params['sql'] = $sql;
+        $params['return'] = true;
+        $params['type'] = "SQL";
+
+        return $params;
+    }
+
+    protected function getVoteCountBadgesParams($data)
+    {
+        $sql  = "insert into kort.user_badge (user_id, badge_id) ";
+        $sql .= "select u.id, b.badge_id ";
+        $sql .= "from   kort.badge b, kort.user_model u ";
+        $sql .= "where  u.id = " . $data['user_id'] . " ";
+        $sql .= "and b.compare_value <= u.vote_count ";
+        $sql .= "and b.name like 'vote_count_%' ";
+        $sql .= "and not exists (select 1 from kort.user_badge ub where ub.user_id = u.id and ub.badge_id = b.badge_id)";
+        $sql .= "returning badge_id";
+
+        $params = array();
+        $params['sql'] = $sql;
+        $params['return'] = true;
+        $params['type'] = "SQL";
+
+        return $params;
+    }
+
+    protected function getFixCountBadgesParams($data)
+    {
+        $sql  = "insert into kort.user_badge (user_id, badge_id) ";
+        $sql .= "select u.id, b.badge_id ";
+        $sql .= "from   kort.badge b, kort.user_model u ";
+        $sql .= "where  u.id = " . $data['user_id'] . " ";
+        $sql .= "and b.compare_value <= u.fix_count ";
+        $sql .= "and b.name like 'fix_count_%' ";
+        $sql .= "and not exists (select 1 from kort.user_badge ub where ub.user_id = u.id and ub.badge_id = b.badge_id)";
+        $sql .= "returning badge_id";
+
+        $params = array();
+        $params['sql'] = $sql;
+        $params['return'] = true;
+        $params['type'] = "SQL";
+
+        return $params;
+    }
+
+    protected function getCompletedParams($data)
+    {
+        $sql  = "update kort.fix set ";
+        $sql .= "complete = (select required_validations - upratings + downratings >= 0 from kort.validations where id = " . $data['fix_id'] . ") ";
+        $sql .= "where  u.fix_id = " . $data['fix_id'] . " ";
+        $sql .= "returning complete";
+
+        $params = array();
+        $params['sql'] = $sql;
+        $params['return'] = true;
+        $params['type'] = "SQL";
 
         return $params;
     }
@@ -57,37 +115,42 @@ class VoteHandler extends DbProxyHandler
     {
         $transProxy = new \Webservice\TransactionDbProxy();
 
-        $insertVoteParams = $this->insertVoteParams($data);
+        $insertVoteParams = $this->insertParams($data);
+        $highscoreBadgesParams = $this->getHighscoreBadgesParams($data);
+        $fixCountBadgesParams = $this->getFixCountBadgesParams($data);
+        $voteCountBadgesParams = $this->getVoteCountBadgesParams($data);
         $updateKoinCountParams = $this->updateKoinCointParams($data);
+        $completedParams = $this->getCompletedParams($data);
 
         $transProxy->addToTransaction($insertVoteParams);
+        $transProxy->addToTransaction($highscoreBadgesParams);
+        $transProxy->addToTransaction($fixCountBadgesParams);
+        $transProxy->addToTransaction($voteCountBadgesParams);
         $transProxy->addToTransaction($updateKoinCountParams);
+        $transProxy->addToTransaction($completedParams);
+        $result = json_decode($transProxy->sendTransaction());
 
+        $insertedHighscoreBadges = (!empty($result[1])) ? json_decode($result[1], true) : null;
+        $highscoreBadgeId = $insertedHighscoreBadges[0]['badge_id'];
+        $insertedFixCountBadges = (!empty($result[2])) ? json_decode($result[2], true) : null;
+        $fixCountBadgeId = $insertedFixCountBadges[0]['badge_id'];
+        $insertedVoteCountBadges = (!empty($result[3])) ? json_decode($result[3], true) : null;
+        $voteCountBadgeId = $insertedVoteCountBadges[0]['badge_id'];
+        $koins = json_decode($result[4], true);
 
-
-        //$bugHandler = new \Webservice\Bug\BugHandler();
-        //$selectError = $bugHandler->getByFixId($data['fix_id']);
-
-        $vote = $transProxy->sendTransaction();
-
-        //return $reward;
-
-        //1. query insert vote
-        //2. berechne badges + koins
-        //3. badges hinzufügen / koins dazuzählen
-        //4.
-
-        if (!$vote) {
-            return false;
+        $badges = array();
+        if (!empty($highscoreBadgeId)) {
+            $badges[] = Badge::findById($highscoreBadgeId);
         }
-
-        return $vote;
-
-        $koinCountTotal = $vote['koin_count_total'];
-        $koinCountNew = $vote['koin_count_new'];
-        $firstBadge = new Badge("highscore_place_1");
-        $voteBadge = new Badge("vote_count_10");
-        $reward = new Reward($koinCountTotal, $koinCountNew, array($firstBadge, $voteBadge));
+        if (!empty($fixCountBadgeId)) {
+            $badges[] = Badge::findById($fixCountBadgeId);
+        }
+        if (!empty($voteCountBadgeId)) {
+            $badges[] = Badge::findById($voteCountBadgeId);
+        }
+        $koinCountTotal = $koins['koin_count_total'];
+        $koinCountNew = $koins['koin_count_new'];
+        $reward = new Reward($koinCountTotal, $koinCountNew, $badges);
         return $reward->toJson();
     }
 }
