eaa9cdeae2708371d3d6ded32d6ea56ce9f86713
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-09 00:20:15 +0100

    Added PHPDoc comments

--- server/php/Webservice/Fix/FixHandler.php
+++ server/php/Webservice/Fix/FixHandler.php
@@ -1,22 +1,41 @@
 <?php
+/**
+ * kort - Webservice\Fix\FixHandler class
+ */
 namespace Webservice\Fix;
 
 use Webservice\DbProxyHandler;
 use Model\Badge;
 use Model\Reward;
 
+/**
+ * The FixHandler handles request to the fix webservice
+ */
 class FixHandler extends DbProxyHandler
 {
+    /**
+     * Returns the table used by this handler.
+     * @return string the table name used by this handler.
+     */
     protected function getTable()
     {
         return 'kort.fix';
     }
 
+    /**
+     * Returns the table fields used by this handler.
+     * @return array the table fields used by this handler.
+     */
     protected function getFields()
     {
         return array('user_id', 'error_id', 'schema', 'osm_id', 'message');
     }
 
+    /**
+     * Saves a fix in the database and gives the user a reward for this action.
+     * @param array $data the fix data
+     * @return string|bool return the JSON-encoded reward for the user of successful, false otherwise
+     */
     public function insertFix($data)
     {
         $insertedFix = $this->getDbProxy()->insert($data);
--- server/php/Webservice/TransactionDbProxy.php
+++ server/php/Webservice/TransactionDbProxy.php
@@ -1,17 +1,32 @@
 <?php
+/**
+ * kort - Webservice\TransactionDbProxy class
+ */
 namespace Webservice;
 
 use Webservice\DbProxy;
 
+/**
+ * The TransactionDbProxy class is a special DbProxy which runs several statements
+ * in one request in one transaction
+ */
 class TransactionDbProxy extends DbProxy
 {
+    /** @var array the different statements to run in the transaction */
     protected $statements = array();
 
+    /**
+     * Creates a new TransactionDbProxy object
+     */
     public function __construct()
     {
         parent::__construct(null, array());
     }
 
+    /**
+     * Makes a request for the whole transaction
+     * @return array array with an entry for each statments result.
+     */
     public function sendTransaction()
     {
         $path  = "/transaction";
@@ -25,16 +40,27 @@ class TransactionDbProxy extends DbProxy
         return $result;
     }
 
+    /**
+     * Indicates wheter the requests runs in a transaction or not
+     * @return bool true if the request runs in a transation, false otherwise
+     */
     public function isTransaction()
     {
         return true;
     }
 
+    /**
+     * Removed all statements from this object
+     */
     protected function clearTransaction()
     {
         $this->statements = array();
     }
 
+    /**
+     * Adds a new statement as a parameter array to the transaction
+     * @param array $params a parameter object describing the statement to execute
+     */
     public function addToTransaction($params)
     {
         $this->statements[] = $params;
