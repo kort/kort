392aa6b1bf246ba96f04a533239bba9b9137c06f
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-12-09 17:24:39 +0100

    fixed view dependency

--- app/view/Main.js
+++ app/view/Main.js
@@ -5,7 +5,14 @@ Ext.define('Kort.view.Main', {
     extend: 'Ext.tab.Panel',
     xtype: 'main',
     id: 'mainTabPanel',
-
+    requires: [
+        'Kort.view.bugmap.NavigationView',
+        'Kort.view.validation.NavigationView',
+        'Kort.view.highscore.Container',
+        'Kort.view.profile.Container',
+        'Kort.view.about.Container'
+    ],
+    
     config: {
         tabBar: {
             docked: 'bottom',
