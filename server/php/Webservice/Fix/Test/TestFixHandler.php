a4a985ef07485c5d3179e06a7d7bfe4ad86b9339
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-10 17:10:24 +0100

    Fixed broken tests

--- server/php/Webservice/Fix/Test/TestFixHandler.php
+++ server/php/Webservice/Fix/Test/TestFixHandler.php
@@ -31,7 +31,13 @@ class TestFixHandler extends AbstractKortUnitTestCase
 
     public function testInsertFix()
     {
-        $data = array("test" => "data");
+        $data = array(
+            "test" => "data",
+            "user_id" => 0,
+            "error_id" => 0,
+            "schema" => "95",
+            "osm_id" => 898213791823
+        );
         $this->mockProxy->shouldReceive('insert');
         $this->assertNotNull($this->handler->insertFix($data));
     }
