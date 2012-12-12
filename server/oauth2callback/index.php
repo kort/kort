6b76d5cc8b98973e36a236b40d85e9fe1461aaf7
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-12 23:57:28 +0100

    Refactoring of OAuth functionality in AbstractOAuthCallback and GoogleOAuth

--- server/oauth2callback/index.php
+++ server/oauth2callback/index.php
@@ -1,57 +1,25 @@
 <?php
 /**
- * kort - the OAuth callback for Google
+ * kort - The OAuth callback for Google
  */
 
-/** Load the Google API Client */
+// Load the Google API Client
 require_once "../../lib/google-api-php-client/src/Google_Client.php";
-/** Load the Google OAuth 2.0 service */
+// Load the Google OAuth 2.0 service
 require_once '../../lib/google-api-php-client/src/contrib/Google_Oauth2Service.php';
-/** Load the ClassLoader */
+// Load the ClassLoader
 require_once('../php/ClassLoader.php');
 
 Kort\ClassLoader::registerAutoLoader();
-$client = new Google_Client();
-$oauth2 = new Google_Oauth2Service($client);
+$googleOAuth = new \OAuth\GoogleOAuth();
 
-//refresh user info
 if (isset($_GET['code'])) {
-    $client->authenticate($_GET['code']);
+    $googleOAuth->authenticate($_GET['code']);
+    $dbUser = $googleOAuth->saveApplicationUser();
+
     session_start();
-    $token = $client->getAccessToken();
-    $user = $oauth2->userinfo->get();
-    $user['oauth_provider'] = "Google";
-    $user['oauth_user_id'] = $user['email'];
-
-    $userGetHandler = new Webservice\User\UserGetHandler();
-    $userData = json_decode($userGetHandler->getUserByOAuthUserId($user['email']), true);
-
-    $userHandler = new Webservice\User\UserHandler();
-    if (empty($userData)) {
-        //generate user secret
-        $generator = new Helper\SecretGenerator();
-        $user['secret'] = $generator->getSecret();
-        $authObj = json_decode($token);
-        if (isset($authObj->refresh_token)) {
-            $user['token'] = $authObj->refresh_token;
-            $fields[] = "token";
-        }
-        $dbUser = $userHandler->insertUser($user);
-    } else {
-        $dbUser = $userHandler->updateUser($userData['id'], $user);
-    }
-    $dbUser = json_decode($dbUser, true);
     $_SESSION['secret'] = $dbUser['secret'];
     $_SESSION['user_id'] = $dbUser['user_id'];
 }
 
-$appUrl = 'http://' . $_SERVER['HTTP_HOST'];
-if ($_SERVER['HTTP_HOST'] === "localhost") {
-    $appUrl .= "/kort";
-
-    if (isset($_GET['state']) && !empty($_GET['state'])) {
-        $appUrl .= "/build/Kort/" . $_GET['state'] . "/";
-    }
-}
-
-header('Location: ' . $appUrl);
+header('Location: ' . $googleOAuth->getApplicationUrl());
--- server/php/OAuth/GoogleOAuth.php
+++ server/php/OAuth/GoogleOAuth.php
@@ -7,7 +7,105 @@ namespace OAuth;
 /**
  * The GoogleOAuth class takes care about the login with Google.
  */
-class GoogleOAuth implements IOAuthCallback
+class GoogleOAuth extends AbstractOAuthCallback
 {
-    //put your code here
+    /**
+     * The Google API Client.
+     *
+     * @var Google_Client
+     */
+    protected $client;
+
+    /**
+     * The Google OAuth Service to receive user data.
+     *
+     * @var Google_Oauth2Service
+     */
+    protected $oAuthService;
+
+    /**
+     * The current user.
+     *
+     * @var array
+     */
+    protected $user = null;
+
+    /**
+     * Creates a new instance of GoogleOAuth.
+     */
+    public function __construct()
+    {
+        $this->client = new \Google_Client();
+        $this->oAuthService = new \Google_Oauth2Service($this->client);
+    }
+
+    /**
+     * Authenticate against the OAuth provider.
+     *
+     * @param string $code The autentication code from Google.
+     *
+     * @return void
+     */
+    public function authenticate($code)
+    {
+        $this->client->authenticate($code);
+    }
+
+    /**
+     * Returns the OAuth access token for this session.
+     *
+     * @return string The OAuth access token for this session
+     */
+    protected function getAccessToken()
+    {
+        return $this->client->getAccessToken();
+    }
+
+    /**
+     * Returns the OAuth refresh token, which typically lives a long time.
+     *
+     * @return string The OAuth refresh token
+     */
+    public function getRefreshToken()
+    {
+        $authObj = json_decode($this->getAccessToken());
+        if (isset($authObj->refresh_token)) {
+            return $authObj->refresh_token;
+        }
+        return null;
+    }
+
+    /**
+     * Returns an array with user infos from the OAuth provier.
+     *
+     * @return array The current user array
+     */
+    public function getOAuthUser()
+    {
+        if (empty($this->user)) {
+            $this->user = $this->oAuthService->userinfo->get();
+        }
+        return $this->user;
+    }
+
+    /**
+     * Returns an identifier for this OAuth user.
+     *
+     * @return mixed Identifier for this OAuth user
+     */
+    protected function getOAuthUserId()
+    {
+        $user = $this->getOAuthUser();
+        return $user['email'];
+    }
+
+    /**
+     * The name of the OAuth provider.
+     *
+     * @return string Name of the OAuth provider
+     */
+    public function getOAuthProvider()
+    {
+        return "Google";
+    }
 }
