379645f8f9ae57458ff21fecb76ac75250a1262a
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-12-13 10:37:48 +0100

    fixed some phpdoc comments

--- server/webservices/user/index.php
+++ server/webservices/user/index.php
@@ -3,7 +3,9 @@
  * kort - the /user webservices
  */
 
+/** Load Slim library */
 require_once('../../../lib/Slim-2.1.0/Slim/Slim.php');
+/** Load ClassLoader */
 require_once('../../../server/php/ClassLoader.php');
 
 use Webservice\User\UserHandler;
