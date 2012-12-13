68ff2971824a231c7d1d1b60ba420e11e4fd2038
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-12-13 13:37:13 +0100

    added further information to about screen

--- app/view/about/Container.js
+++ app/view/about/Container.js
@@ -30,13 +30,21 @@ Ext.define('Kort.view.about.Container', {
                             '<dl class="kort-definitionlist info">' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.version.title') + '</dt>' +
                                 '<dd>' + Kort.util.Config.getVersion() + '</dd>' +
-                                '<dt>' + Ext.i18n.Bundle.message('about.authors.title') + '</dt>' +
-                                '<dd>' +  Kort.util.Config.getAbout().authors[0] + '</dd>' +
-                                '<dd>' +  Kort.util.Config.getAbout().authors[1] + '</dd>' +
+                                '<dt>' + Ext.i18n.Bundle.message('about.information.title') + '</dt>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.information.homepage') + ' ' + Kort.util.Config.getAbout().information[0] + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.information.bugs') + ' ' + Kort.util.Config.getAbout().information[1] + '</dd>' +
+                                '<dt>' + Ext.i18n.Bundle.message('about.developers.title') + '</dt>' +
+                                '<dd>' +  Kort.util.Config.getAbout().developers[0] + '</dd>' +
+                                '<dd>' +  Kort.util.Config.getAbout().developers[1] + '</dd>' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.project.title') + '</dt>' +
-                                '<dd>' + Kort.util.Config.getAbout().project + '</dd>' +
-                                '<dt>' + Ext.i18n.Bundle.message('about.partners.title') + '</dt>' +
-                                '<dd>' + Kort.util.Config.getAbout().partners[0] + '</dd>' +
+                                '<dd>' + Kort.util.Config.getAbout().project.title + '</dd>' +
+                                '<dd>' + Kort.util.Config.getAbout().project.school + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.project.advisor') + ' ' + Kort.util.Config.getAbout().project.advisor + '</dd>' +
+                                '<dt>' + Ext.i18n.Bundle.message('about.credits.title') + '</dt>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.credits.partner') + ' ' + Kort.util.Config.getAbout().credits.partner + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.credits.mapdata') + ' ' + Kort.util.Config.getAbout().credits.mapdata + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.credits.tiledata') + ' ' + Kort.util.Config.getAbout().credits.tiledata + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.credits.markers') + ' ' + Kort.util.Config.getAbout().credits.markers + '</dd>' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.legal.title') + '</dt>' +
                                 '<dd>' + Ext.i18n.Bundle.message('about.legal.message') + '</dd>' +
                             '</dl>' +
