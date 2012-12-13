045ae5946e5c8ed8bb044fa63da19fd42570ce63
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-13 08:58:54 +0100

    Updated test for insertFix() in FixHandler

--- server/php/Webservice/Fix/Test/TestFixHandler.php
+++ server/php/Webservice/Fix/Test/TestFixHandler.php
@@ -14,9 +14,36 @@ class TestFixHandler extends AbstractKortUnitTestCase
 
     public function setUp()
     {
-        $this->mockProxy = M::mock('\Webservice\DbProxy');
-        $this->handler = new FixHandler();
-        $this->handler->setDbProxy($this->mockProxy);
+        $this->mockProxy = M::mock('Webservice\TransactionDbProxy');
+        $this->handler = new FixHandler($this->mockProxy);
+
+        $this->sendTransResult = array();
+        $this->sendTransResult[0] = array(
+            "user_id"  => 0,
+            "error_id" => 976576,
+            "schema"   => "95",
+            "osm_id"   => 898213791823,
+            "message"  => "grade4"
+        );
+        $this->sendTransResult[1] = array();
+        $this->sendTransResult[2][0] = array(
+            "badge_id" => 10,
+            "create_date" => "2012-12-13 00:22:12.300417"
+        );
+        $this->sendTransResult[3] = array();
+        $this->sendTransResult[4][0] = array(
+            "koin_count_total" => 260,
+            "koin_count_new"   => 5
+        );
+        $this->sendTransResultJson = json_encode($this->sendTransResult);
+
+        $this->reward = array();
+        $this->reward["badges"][] = array(
+            "name" => "fix_count_1"
+        );
+        $this->reward["koin_count_new"] = 5;
+        $this->reward["koin_count_total"] = 260;
+        $this->rewardJson = json_encode($this->reward);
     }
 
     public function tearDown()
@@ -34,11 +61,19 @@ class TestFixHandler extends AbstractKortUnitTestCase
         $data = array(
             "test" => "data",
             "user_id" => 0,
-            "error_id" => 0,
+            "error_id" => 976576,
             "schema" => "95",
             "osm_id" => 898213791823
         );
-        $this->mockProxy->shouldReceive('insert');
-        $this->assertNotNull($this->handler->insertFix($data));
+
+
+        $this->mockProxy->shouldReceive('addToTransaction')->andReturn(1)->once();
+        $this->mockProxy->shouldReceive('addToTransaction')->andReturn(2)->once();
+        $this->mockProxy->shouldReceive('addToTransaction')->andReturn(3)->once();
+        $this->mockProxy->shouldReceive('addToTransaction')->andReturn(4)->once();
+        $this->mockProxy->shouldReceive('addToTransaction')->andReturn(5)->once();
+
+        $this->mockProxy->shouldReceive('sendTransaction')->once()->andReturn($this->sendTransResultJson);
+        $this->assertEqual($this->rewardJson, $this->handler->insertFix($data));
     }
 }
