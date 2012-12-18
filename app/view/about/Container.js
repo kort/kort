809958ccef6c12f75d92b804ea2cbef0042e8cc7
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-12-18 12:29:24 +0100

    added feedback form to about page

--- app/view/about/Container.js
+++ app/view/about/Container.js
@@ -32,7 +32,8 @@ Ext.define('Kort.view.about.Container', {
                                 '<dd>' + Kort.util.Config.getVersion() + '</dd>' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.information.title') + '</dt>' +
                                 '<dd>' + Ext.i18n.Bundle.message('about.information.homepage') + ' ' + Kort.util.Config.getAbout().information[0] + '</dd>' +
-                                '<dd>' + Ext.i18n.Bundle.message('about.information.bugs') + ' ' + Kort.util.Config.getAbout().information[1] + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.information.feedback') + ' ' + Kort.util.Config.getAbout().information[1] + '</dd>' +
+                                '<dd>' + Ext.i18n.Bundle.message('about.information.bugs') + ' ' + Kort.util.Config.getAbout().information[2] + '</dd>' +
                                 '<dt>' + Ext.i18n.Bundle.message('about.developers.title') + '</dt>' +
                                 '<dd>' +  Kort.util.Config.getAbout().developers[0] + '</dd>' +
                                 '<dd>' +  Kort.util.Config.getAbout().developers[1] + '</dd>' +
