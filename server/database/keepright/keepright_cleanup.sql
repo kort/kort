04fd97588c6b957219ac0d41ec212b89714a9cd3
Stefan Oderbolz <oderbolz@gmail.com>
2012-12-30 17:24:47 +0100

    Keep at least all European errors

--- server/database/keepright/keepright_cleanup.sql
+++ server/database/keepright/keepright_cleanup.sql
@@ -1,5 +1,5 @@
 -- reduce amount of data
-delete from keepright.errors where schema::numeric < 80;
+delete from keepright.errors where schema::numeric between 20 and 75;
 
 vacuum full keepright.errors;
 reindex database osm_bugs;
