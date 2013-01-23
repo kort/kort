194820c98d81f57d9270d8d9b05042f8f9cdd8b7
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-01-24 00:44:00 +0100

    Extended use of data, now only schema 26 to 45 (USA) are getting cleaned up

--- server/database/keepright/keepright_cleanup.sql
+++ server/database/keepright/keepright_cleanup.sql
@@ -1,5 +1,5 @@
 -- reduce amount of data
-delete from keepright.errors where schema::numeric between 20 and 75;
+delete from keepright.errors where schema::numeric between 26 and 45;
 
 vacuum full keepright.errors;
 reindex database osm_bugs;
