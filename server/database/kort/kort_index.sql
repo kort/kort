c933b233edc6b3ddbad1c636c5b7b288505c0486
Stefan Oderbolz <oderbolz@gmail.com>
2013-03-04 01:24:15 +0000

    Removed unused index

--- server/database/kort/kort_index.sql
+++ server/database/kort/kort_index.sql
@@ -1,3 +1,2 @@
 CREATE INDEX answer_type_idx ON kort.answer (type ASC NULLS LAST);
-CREATE INDEX error_type_id_idx ON kort.errors (error_type_id ASC NULLS LAST);
 CREATE INDEX error_name_idx ON kort.error_type (type ASC NULLS LAST);
