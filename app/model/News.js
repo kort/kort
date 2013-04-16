9934aa5b99f9c604000f65c0a6556da1ba446414
Fausto <carmelo@carmelo.ch>
2013-04-16 11:48:36 +0200

    updated news spec

--- app/model/News.js
+++ app/model/News.js
@@ -4,17 +4,18 @@
 Ext.define('Kort.model.News', {
     extend: 'Ext.data.Model',
     config: {
-        idProperty: 'id',
+        idProperty: 'newsid',
 
         fields: [
-            { name: 'id', type: 'auto' },
+            { name: 'newsid', type: 'auto' },
+            { name: 'feedtitle', type: 'string'},
+            { name: 'feedid', type: 'auto'},
             { name: 'lang', type: 'string'},
             { name: 'title', type: 'string' },
             { name: 'content', type: 'string' },
             { name: 'updated', type: 'date'},
             { name: 'link', type: 'string' },
             { name: 'read', type: 'boolean'}
-
         ]
     }
 });
