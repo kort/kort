24f50935f533d6e03b7ed5a1c75c1f296d9db5f6
annrita <aegli@hsr.ch>
2013-05-04 17:47:31 +0200

    highscoreTest

---
+++ build/Kort/production/test/client/webservices/TestHighscoreWebservice.js
@@ -0,0 +1,20 @@
+module("kort-HighscoreWebservice", {
+    setup: function () {
+        this.path = 'server/webservices/highscore';
+    }
+});
+
+testSkip("get table", function() {
+    var type = 'GET',
+        data = null,
+        url = urlLib.getAppUrl() + '/' + this.path + '/limit';
+
+    api_test(url, type, data, function (highscore) {
+        notStrictEqual(highscore, undefined, "method call failed");
+
+        notStrictEqual(highscore.user_id, undefined, "id field should be defined.");
+        notStrictEqual(highscore.username, undefined, "username field should be defined.");
+        notStrictEqual(highscore.koin_count, undefined, "koin count field should be defined.");
+        notStrictEqual(highscore.ranking, undefined, "ranking field should be defined.");
+    });
+});
---
+++ test/client/webservices/TestHighscoreWebservice.js
@@ -0,0 +1,21 @@
+module("kort-HighscoreWebservice", {
+    setup: function () {
+        this.path = 'server/webservices/highscore';
+    }
+});
+
+test("get table", function() {
+    var type = 'GET',
+        data = null,
+        url = urlLib.getAppUrl() + '/' + this.path + '/limit';
+
+    api_test(url, type, data, function (highscore) {
+        notStrictEqual(highscore, undefined, "method call failed");
+
+        notStrictEqual(highscore.user_id, undefined, "id field should be defined.");
+        notStrictEqual(highscore.username, undefined, "username field should be defined.");
+        notStrictEqual(highscore.koin_count, undefined, "koin count field should be defined.");
+        notStrictEqual(highscore.ranking, undefined, "ranking field should be defined.");
+    });
+});
+
