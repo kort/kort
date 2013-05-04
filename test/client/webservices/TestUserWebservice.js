1550a45ae58679e390389358625226a022fb07b7
annrita <aegli@hsr.ch>
2013-05-04 15:59:41 +0200

    fixing url in original unit tests, adding missing '/'

--- test/client/webservices/TestUserWebservice.js
+++ test/client/webservices/TestUserWebservice.js
@@ -7,7 +7,7 @@ module("kort-UserWebservice", {
 test("get user", function() {
 	var type = 'GET',
         data = null,
-        url = urlLib.getAppUrl() + this.path + '/09821093801923';
+        url = urlLib.getAppUrl() + '/' + this.path + '/09821093801923';
 
     api_test(url, type, data, function (user) {
         notStrictEqual(user, undefined, "method call failed");
@@ -26,7 +26,7 @@ test("create user", function() {
             username: 'testuser',
             email: 'test@kort.ch'
         },
-        url = urlLib.getAppUrl() + this.path + '/';
+        url = urlLib.getAppUrl() + '/' + this.path + '/';
 
     api_test(url, type, data, function (result) {
         console.log(result);
