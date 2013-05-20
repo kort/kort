cbad5914356f75c5c14f6e5ec729da96d12c3297
Fausto <carmelo@carmelo.ch>
2013-05-20 23:44:30 +0200

    updated osm_errors logic

--- server/database/osm_errors/queries/osm_errors_missingcuisine.sql
+++ server/database/osm_errors/queries/osm_errors_missingcuisine.sql
@@ -1,6 +1,6 @@
 INSERT INTO osm_errors.errors (schema, error_type_id, error_name, object_id, object_type, msgid, lat, lon, geom, txt1)
 SELECT 100, 400, 'missing cuisine', osmq.osm_id, 'way', 'Error from EOSMDBOne', osmq.lat, osmq.lon, osmq.geom, osmq.txt1
-FROM dblink('dbname=gis_db port=8080 host=152.96.80.44 user=readonly',
+FROM osm_errors.dblink('dbname=gis_db port=8080 host=152.96.80.44 user=readonly',
      'SELECT ceiling(ST_X(ST_Centroid(way))::numeric * 100) AS lat, ceiling(ST_Y(ST_Centroid(way))::numeric * 100) AS lon, ST_Transform(ST_Centroid(way),4326) AS geom, osm_id, name AS txt1
 			FROM osm_poi
 			WHERE tags @> hstore(''amenity'', ''restaurant'')
