709d1891eb5a7eb0a6499297ed280dfd127bfafc
Stefan Oderbolz <oderbolz@gmail.com>
2013-05-21 23:04:28 +0300

    Added partial index for fix table

--- server/database/kort/kort_index.sql
+++ server/database/kort/kort_index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX answer_type_idx ON kort.answer (type ASC NULLS LAST);
 CREATE INDEX error_name_idx ON kort.error_type (type ASC NULLS LAST);
+CREATE INDEX fix_idx ON fix(error_id, osm_id, schema) WHERE (complete and valid) OR (not complete);
