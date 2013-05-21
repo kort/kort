51340df5a163c9016db83252315c178746455a4b
Stefan Oderbolz <oderbolz@gmail.com>
2013-05-21 22:10:12 +0200

    Added index for vote.user_id

--- server/database/kort/kort_index.sql
+++ server/database/kort/kort_index.sql
@@ -1,3 +1,4 @@
 CREATE INDEX answer_type_idx ON kort.answer (type ASC NULLS LAST);
 CREATE INDEX error_name_idx ON kort.error_type (type ASC NULLS LAST);
-CREATE INDEX fix_idx ON fix(error_id, osm_id, schema) WHERE (complete and valid) OR (not complete);
+CREATE INDEX fix_idx ON kort.fix (error_id, osm_id, schema) WHERE (complete and valid) OR (not complete);
+CREATE INDEX vote_user_idx ON kort.vote (user_id);
