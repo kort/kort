466bffdc1a75803640352d99fbae1835b970bed5
Stefan Oderbolz <oderbolz@gmail.com>
2013-05-21 22:11:29 +0200

    Added index for vote.fix_id

--- server/database/kort/kort_index.sql
+++ server/database/kort/kort_index.sql
@@ -2,3 +2,4 @@ CREATE INDEX answer_type_idx ON kort.answer (type ASC NULLS LAST);
 CREATE INDEX error_name_idx ON kort.error_type (type ASC NULLS LAST);
 CREATE INDEX fix_idx ON kort.fix (error_id, osm_id, schema) WHERE (complete and valid) OR (not complete);
 CREATE INDEX vote_user_idx ON kort.vote (user_id);
+CREATE INDEX vote_fix_idx ON kort.vote (fix_id);
