c684fd500892869ff0a88bdcc62181811f2e8f55
annrita <aegli@hsr.ch>
2013-05-25 15:55:27 +0200

    refactoring campaign -> promotion

--- app/view/map/mission/PromotionOverlay.js
+++ app/view/map/mission/PromotionOverlay.js
@@ -5,6 +5,6 @@ Ext.define('Kort.view.map.mission.PromotionOverlay', {
     alias: 'widget.promotionoverlay',
     extend: 'Ext.Component',
     config: {
-        html: '<div class="bugmap-campaign-overlay" />'
+        html: '<div class="map-promotion-overlay" />'
     }
 });
