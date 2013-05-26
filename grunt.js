1e1e07226efb4818737f497a48644a7527719c1a
Fausto <carmelo@carmelo.ch>
2013-05-26 18:35:43 +0200

    old style grunt file recovered

---
+++ grunt.js
@@ -0,0 +1,76 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        qunit: {
+            files: ['test/client/index.html']
+        },
+        lint: {
+            src: 'app/**/*.js',
+            grunt: 'grunt.js',
+            app: 'app.js*',
+            tests: 'test/client/**/*.js'
+        },
+        jshint: {
+            options: {
+                jquery: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: false,
+                onevar: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                devel: true,
+                trailing: true,
+                white:  false,
+                maxcomplexity: 4
+                /*indent: 4  comment out until white/indent options are separated => https://github.com/jshint/jshint/issues/655 */
+            },
+            globals: {
+                Kort: true,
+                Ext: true,
+                L: true,
+                UrlLib: true
+            },
+            tests: {
+                globals: {
+                    Ext: true,
+                    L: true,
+                    UrlLib: true,
+                    urlLib: true,
+                    asyncTest: true,
+                    api_test: true,
+                    deepEqual: true,
+                    equal: true,
+                    expect: true,
+                    module: true,
+                    notDeepEqual: true,
+                    notEqual: true,
+                    notStrictEqual: true,
+                    ok: true,
+                    QUnit: true,
+                    raises: true,
+                    start: true,
+                    stop: true,
+                    strictEqual: true,
+                    test: true,
+                    testSkip: true
+                }
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'lint qunit concat min');
+
+    // Travis CI task.
+    grunt.registerTask('travis', 'lint qunit');
+};
