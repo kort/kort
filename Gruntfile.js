026096e2c87cd2be93b1dc40e81d3f1620dbdc99
Fausto <carmelo@carmelo.ch>
2013-05-26 18:33:04 +0200

    temp reenables grunt for test env

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,16 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         qunit: {
-            'kort': ['test/client/index.html']
+            files: ['test/client/index.html']
         },
         jshint: {
-            'kort': ['app/**/*.js', 'Grunfile.js', 'app.js*', 'test/client/**/*.js'],
+            files: {
+                src: 'app/**/*.js',
+                grunt: 'grunt.js',
+                app: 'app.js*',
+                tests: 'test/client/**/*.js'
+            },
+
             options: {
                 jquery: true,
                 curly: true,
@@ -27,8 +33,17 @@ module.exports = function(grunt) {
                 trailing: true,
                 white:  false,
                 maxcomplexity: 4,
+                /*indent: 4  comment out until white/indent options are separated => https://github.com/jshint/jshint/issues/655 */
                 globals: {
                     Kort: true,
+                    Ext: true,
+                    L: true,
+                    UrlLib: true
+                }
+            },
+
+            tests: {
+                globals: {
                     Ext: true,
                     L: true,
                     UrlLib: true,
@@ -55,16 +70,16 @@ module.exports = function(grunt) {
         }
     });
 
-    // Load plugins
+    // Default task.
+    //grunt.registerTask('default', 'lint qunit concat min'); //old style v0.3.x
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
-
-    // Default task.
-    grunt.registerTask('default', 'jshint:kort');
-    
-    // Linting task.
-    grunt.registerTask('lint', 'jshint:kort');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.registerTask('default', ['lint']);
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']); //new style v0.4.x
 
     // Travis CI task.
-    grunt.registerTask('travis', 'jshint:kort qunit:kort');
+    //grunt.registerTask('travis', 'lint qunit');
 };
