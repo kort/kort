f2a40cf37484f3e036b5bf2e4294adc5b38060bc
Fausto <carmelo@carmelo.ch>
2013-06-03 00:44:06 +0200

    Update consolidation skript using distinct

--- server/database/all_errors/all_errors_consolidate.sql
+++ server/database/all_errors/all_errors_consolidate.sql
@@ -1,5 +1,5 @@
 INSERT INTO all_errors.errors (source,error_id,schema,error_type_id,osm_id,osm_type,description,latitude,longitude,geom,txt1,txt2,txt3,txt4,txt5)
-SELECT 'keepright' AS source, error_id, schema, error_type_id,object_id AS osm_id, object_type AS osm_type, msgid AS description, lat AS latitude, lon AS longitude, geom, txt1, txt2, txt3, txt4, txt5
+SELECT DISTINCT 'keepright' AS source, error_id, schema, error_type_id,object_id AS osm_id, object_type AS osm_type, msgid AS description, lat AS latitude, lon AS longitude, geom, txt1, txt2, txt3, txt4, txt5
 FROM keepright.errors
 WHERE ((state = ANY (ARRAY['new'::keepright.state, 'reopened'::keepright.state])) AND (object_id <> ALL (ARRAY[(1611867263)::bigint, (1723313154)::bigint, (111841602)::bigint])))
 UNION ALL
